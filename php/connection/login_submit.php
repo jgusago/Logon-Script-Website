be6472c7600c098871a20715279c520dcbdaff98
orlancastelo <casteloorlan@gmail.com>
2018-08-30 19:51:47 +0800

    Edit
    Index.php test function

---
+++ php/connection/login_submit.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include("php/connection.php");
+  
+$un = $_POST['username'];
+$pw = $_POST['password'];
+
+//PDO
+//$stmt = $db->prepare("SELECT * from tbl_users WHERE username=? AND password=?");
+//$stmt->execute(array($un, $pw));
+//$row_count = $stmt->rowCount();
+
+//MySQLi
+<?php
+$sql = "SELECT * from tbl_users WHERE username=? AND password=?";
+$result = $conn->query($sql);
+
+if($row_count >0)
+{
+$_SESSION['username'] = $un;
+header("Location: ../.superadmin.html");
+}
+else
+{
+header("Location: ../index.php?msg=wrong");
+}
+?>
+                                     
