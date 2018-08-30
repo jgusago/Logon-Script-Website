ed89ac91c2c1f53f1083233533422e9fe4f44114
orlancastelo <casteloorlan@gmail.com>
2018-08-31 00:40:05 +0800

    Edit login_submit
    test

--- php/connection/login_submit.php
+++ php/connection/login_submit.php
@@ -5,21 +5,21 @@ include("connection.php");
        if (isset($_POST['username']))
 		{
 			$username = mysqli_real_escape_string($con, $_POST['username']);
-			$password = mysqli_real_escape_string($con, $_POST['password']);
+			$password = mysqli_real_escape_string($con, $_POST['pass']);
 			
-			$query 		= mysqli_query($con, "SELECT * FROM tbl_user WHERE  username='$username' and password='$password'");
+			$query 		= mysqli_query($con, "SELECT * FROM tbl_user WHERE  username='$username' and pass='$password'");
 			$row		= mysqli_fetch_array($query);
 			$num_row 	= mysqli_num_rows($query);
 			
-			if ($num_row = 0) 
+			if ($num_row > 0) 
 				{			
 					$_SESSION['username']=$row['username'];
-					header("Location: index.php?msg=wrong");
+					header('Location: ../../superadmin.php');
 					
 				}
 			else
 				{
-					header('Location: ../../superadmin.php');
+					header("Location: index.php?msg=wrong");
 				}
     }  
     ?>                                              
