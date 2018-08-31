8a2c4cb5a5eb6e7c725d56bbe5a4ee35234725e5
orlancastelo <casteloorlan@gmail.com>
2018-08-31 20:58:49 +0800

    test

--- php/connection/login_submit.php
+++ php/connection/login_submit.php
@@ -1,10 +1,11 @@
 <?php
 	session_start();
-	require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/connection.php";
+	//include("../config/db_connection.php");
+	require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/test_connection.php";
   
 		$un = $_POST['username'];
 		$pw = $_POST['password'];
-		$stmt = $con->prepare("SELECT * from tbl_user WHERE username=? AND password=?");
+		$stmt = $db->prepare("SELECT * from tbl_user WHERE username=? AND password=?");
 		$stmt->execute(array($un, $pw));
 		$row_count = $stmt->rowCount();
 			if($row_count >0)
