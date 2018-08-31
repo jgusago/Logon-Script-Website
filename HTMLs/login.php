28ee805d66b87aa096f70d1987356ca0c9990ade
orlancastelo <casteloorlan@gmail.com>
2018-08-31 22:35:41 +0800

    Edit
    login.php

--- HTMLs/login.php
+++ HTMLs/login.php
@@ -97,7 +97,7 @@ if(isset($_SESSION["username"])) {
                         }
                 ?>
                 </div>
-        <form action="admin_viewing.php" method="POST">
+        <form action="../php/connection/login_submit.php" method="POST">
             <div class="row">
 				<div class="col-sm-12 col-md-10  col-md-offset-1">
 					<div class="form-group">
