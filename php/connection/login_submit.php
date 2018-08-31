37eff587792b4700e0d9021a0c2b6547e99a84f3
orlancastelo <casteloorlan@gmail.com>
2018-08-31 22:50:25 +0800

    edit login.php

--- php/connection/login_submit.php
+++ php/connection/login_submit.php
@@ -8,10 +8,10 @@
 		$stmt = $db->prepare("SELECT * from tbl_user WHERE username=? AND password=?");
 		$stmt->execute(array($un, $pw));
 		$row_count = $stmt->rowCount();
-			if($row_count >0)
+			if($row_count > 0)
 			{
 				$_SESSION['username'] = $un;
-				header("Location: ../../HTMLs/admin_viewing.php");
+				header("Location: admin_viewing.php");
 			}
 			else
 			{
