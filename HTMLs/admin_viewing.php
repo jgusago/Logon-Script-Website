f2825479f817258f190a6e7573ddb8d0df273091
orlancastelo <casteloorlan@gmail.com>
2018-08-31 23:36:26 +0800

    edit admin_viewing.php

--- HTMLs/admin_viewing.php
+++ HTMLs/admin_viewing.php
@@ -235,7 +235,7 @@
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default" data-dismiss="modal" style="font-size:15px;">Cancel</button>
-                            <a class="btn btn-success" href="index.html" style="font-size:15px;"><i class="glyphicon glyphicon-log-out"> Logout</i></a>
+                            <a class="btn btn-success" href="login.php" style="font-size:15px;"><i class="glyphicon glyphicon-log-out"> Logout</i></a>
                         </div>
                     </div>
                 </div>
