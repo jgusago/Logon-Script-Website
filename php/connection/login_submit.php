7bea72586057ccf9d5b3bed6b70e1bb6da3aec59
orlancastelo <casteloorlan@gmail.com>
2018-08-31 23:04:17 +0800

    edit login_submit.php

--- php/connection/login_submit.php
+++ php/connection/login_submit.php
@@ -3,20 +3,20 @@
 	
 	require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
   
-		$un = $_POST['username'];
-		$pw = $_POST['password'];
-		$stmt = $db->prepare("SELECT * from tbl_user WHERE username=? AND password=?");
-		$stmt->execute(array($un, $pw));
-		$row_count = $stmt->rowCount();
-			if($row_count > 0)
-			{
-				$_SESSION['username'] = $un;
-				header("Location: ../../HTMLs/admin_viewing.php");
-			}
-			else
-			{
-				header("Location: ../../HTMLs/login.php?msg=wrong");
-			}
+	$un = $_POST['username'];
+	$pw = $_POST['password'];
+	$stmt = $db->prepare("SELECT * from tbl_users WHERE username=? AND password=?");
+	$stmt->execute(array($un, $pw));
+	$row_count = $stmt->rowCount();
+	if($row_count >0)
+	{
+	$_SESSION['username'] = $un;
+	header("Location: ../../admin_viewing.php");
+	}
+	else
+	{
+	header("Location: login.php?msg=wrong");
+	}
 ?>
                                      
                                      
