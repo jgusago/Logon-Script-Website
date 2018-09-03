d16051b8034cefe2d26e0c9ab7bd52542e005f41
orlancastelo <1040363@dt.com>
2018-09-04 07:41:37 +0800

    edit login_submit.php

--- php/connection/login_submit.php
+++ php/connection/login_submit.php
@@ -3,27 +3,27 @@
 	
 	require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
   
-	$un = $_POST['username'];
-    $pw = $_POST['password'];
-    $pw = $_REQUEST['password']
-	
-    $stmt = $db->prepare("SELECT * from tbl_user WHERE username=? AND password=?");
-	$stmt->execute(array($un, $pw));
-    $row_count = $stmt->rowCount();
-   
-    if($un)
-{
-    // If the password they give maches
-    if($un->pass === sha1($un->salt. $_REQUEST['password']))
-    {
-        echo "login success";
-    }
-    else
-    {
-        echo "wrong password";
-    }
-}
+	//$un = $_POST['username'];
+    //$pw = $_POST['password'];
 
+	
+    //$stmt = $db->prepare("SELECT * from tbl_user WHERE username=? AND password=?");
+	//$stmt->execute(array($un, $pw));
+    //$row_count = $stmt->rowCount();
+   
+    $sql = "SELECT * FROM tbl_user WHERE username = ?";
+    $stmt = $db->prepare($sql);
+    $result = $stmt->execute([$_POST['username']]);
+    $un = $result->fetchAll();
+        if (isset($un[0]) {
+        if (password_verify($_POST['password'], $un[0]->password) {
+            echo "login success";
+        } else {
+            echo "wrong password";
+        }
+        } else {
+        // invalid username
+        }
   
     //if($row_count >0)
         //{
