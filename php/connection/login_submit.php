d0e9d4cf012b5f5df161797781642190fedc7471
orlancastelo <1040363@dt.com>
2018-09-04 06:20:27 +0800

    edit login_submit.php

--- php/connection/login_submit.php
+++ php/connection/login_submit.php
@@ -7,13 +7,12 @@
 	$pw = $_POST['password'];
 	
     //$stmt = $db->prepare("SELECT * from tbl_user WHERE username=? AND password=?");
-    $un = query('SELECT * FROM `tbl_user` WHERE username = ?', array($_REQUEST['username']));
-    //$stmt = $db->prepare("SELECT * FROM `tbl_user` WHERE username = ?', array($_REQUEST['username'])");
+    $stmt = $db->prepare('SELECT * FROM `tbl_user` WHERE username = ?', array($_REQUEST['username']));
 	$stmt->execute(array($un, $pw));
 	$row_count = $stmt->rowCount();
 
 	if($un)
-{
+    {
     // If the password they give maches
     if($un->pass === sha1($un->salt. $_REQUEST['password']))
     {
