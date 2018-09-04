6163493fe065ea3cdacf6c6c7bc03eee361d5155
orlancastelo <1040363@dt.com>
2018-09-05 01:15:47 +0800

    test login

--- php/connection/login_submit.php
+++ php/connection/login_submit.php
@@ -3,34 +3,34 @@
 	
 	require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
   
-	$un = $_POST['username'];
-    $pw = $_POST['password'];
+	//$un = $_POST['username'];
+    //$pw = $_POST['password'];
 
 	
-    $stmt = $db->prepare("SELECT * from tbl_user WHERE username=? AND password=?");
-	$stmt->execute(array($un, $pw));
-    $row_count = $stmt->rowCount();
+    //$stmt = $db->prepare("SELECT * from tbl_user WHERE username=? AND password=?");
+	//$stmt->execute(array($un, $pw));
+    //$row_count = $stmt->rowCount();
    
-    //$sql = "SELECT * FROM tbl_user WHERE username = ?";
-    //$stmt = $db->prepare($sql);
-    //$result = $stmt->execute([$_POST['username']]);
-    //$un = $result->fetchAll();
-        //if (isset($un[0]) {
-        //if (password_verify($_POST['password'], $un[0]->password) {
-            //echo "login success";
-        //} else {
-            //echo "wrong password";
+    $sql = "SELECT * FROM tbl_user WHERE username = ?";
+    $stmt = $db->prepare($sql);
+    $result = $stmt->execute([$_POST['username']]);
+    $un = $result->fetchAll();
+        if (isset($un[0]) {
+        if (password_verify($_POST['password'], $un[0]->password) {
+            echo "login success";
+        } else {
+            echo "wrong password";
+        }
+        } 
+    //if($row_count >0)
+        //{
+            //$_SESSION['username'] = $un;
+            //header("Location: ../../HTMLs/admin_viewing.php");
         //}
-        //} 
-    if($row_count >0)
-        {
-            $_SESSION['username'] = $un;
-            header("Location: ../../HTMLs/admin_viewing.php");
-        }
-    else
-        { 
-            header("Location: ../../HTMLs/login.php?msg=wrong");
-        }
+    //else
+        //{ 
+            //header("Location: ../../HTMLs/login.php?msg=wrong");
+        //}
 ?>
                                      
                                      
