23d7f835a306584a030af7e297587201b13c0a73
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-09-06 10:49:45 +0800

    change schema

--- php/connection/db_connection.php
+++ php/connection/db_connection.php
@@ -4,7 +4,7 @@ $db = new PDO('mysql:host=172.16.39.241;dbname=sky;charset=utf8mb4', 'administra
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 */
-    con = mysqli_connect('172.16.39.241','administrator','Agsmc999','sky');
+    con = mysqli_connect('172.16.39.241','administrator','Agsmc999','logonscript');
     if(!$con){
         trigger_error("NO CONNECTION");
     }
