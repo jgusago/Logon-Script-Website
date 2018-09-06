9a547646196170cc3068bd000511d63bb654df83
orlancastelo <1040363@dt.com>
2018-09-07 04:46:33 +0800

    edit user_account_submit.php

--- php/connection/user_account_submit.php
+++ php/connection/user_account_submit.php
@@ -3,7 +3,7 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
         $sql = "INSERT INTO tbl_user (userid, name, department, position, role, password)
          
-        VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["postion"]."', '".$_POST["role"]."', '".$_POST["password"]=md5($_POST['password'])."')";
+        VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password"]=md5($_POST['password'])."')";
         ($db->query($sql));
         header("Location: admin_users.php?msg=correct");
 
