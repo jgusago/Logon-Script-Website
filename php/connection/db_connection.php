b48239de0acdcb32518d124ae0623b8ee7076605
orlancastelo <1040363@dt.com>
2018-09-07 03:02:40 +0800

    edit db_connection.php

--- php/connection/db_connection.php
+++ php/connection/db_connection.php
@@ -1,13 +1,10 @@
 <?php
-/*
-$db = new PDO('mysql:host=172.16.39.241;dbname=sky;charset=utf8mb4', 'administrator', 'Agsmc999');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-*/
-    $con = mysqli_connect('172.16.39.241','administrator','Agsmc999','logonscript');
+    $db = new PDO('mysql:host=172.16.39.241;dbname=sky;charset=utf8mb4', 'administrator', 'Agsmc999');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    /*$con = mysqli_connect('172.16.39.241','administrator','Agsmc999','logonscript');
     if(!$con){
         trigger_error("NO CONNECTION");
-    }
-
-
-?>
+    }*/
+    ?>
