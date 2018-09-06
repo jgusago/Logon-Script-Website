82500146a6aaf23a02dbaae80df341011a955ab3
orlancastelo <1040363@dt.com>
2018-09-07 05:21:40 +0800

    edit user_account_submit.php

--- php/connection/user_account_submit.php
+++ php/connection/user_account_submit.php
@@ -5,6 +5,6 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
          
         VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password"]=md5($_POST['password'])."')";
         ($db->query($sql));
-        header("Location: admin_users.php?msg=correct");
+        header("Location: ../../iMonitor_Website/admin_users.php?msg=correct");
 
 ?>
