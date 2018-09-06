635ee6e3e259c875dcbeddaaa5ca72a3a0f597c5
John Melvin Gusago <42765192+jgusago@users.noreply.github.com>
2018-09-06 18:40:55 +0800

    index.html changes

---
+++ js/controller/login.js
@@ -0,0 +1,24 @@
+function login(){
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+
+    $.post("php/login_confirm.php",{username:username,password:password},function(data){
+
+        data = data.split("#");
+
+        switch(data(0)){
+            case "success":
+                document.getElementById("login-message").style.display = "block";
+            break;
+
+            case "failed":
+                document.getElementById("login-message").style.display = "block";
+            break;
+
+            default:
+                document.getElementById("login-message").style.display = "block";
+        }
+    });
+
+    return false;
+}
---
+++ php/test/password.php
@@ -0,0 +1,8 @@
+<?php
+
+$password = "Agsmc999";
+
+$ecpassword =  md5(sha1($password));
+
+echo $ecpassword;
+?>
