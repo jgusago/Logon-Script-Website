d0497df7990731592a06b3b6178419f455592f2c
orlancastelo <1040363@dt.com>
2018-09-07 05:19:19 +0800

    edit user_account_submit.php

--- php/connection/user_account_submit.php
+++ php/connection/user_account_submit.php
@@ -1,7 +1,7 @@
 <?php
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
-        $sql = "INSERT INTO sys_user (userid, name, department, position, role, password)
+        $sql = "INSERT INTO tbl_user (userid, name, department, position, role, password)
          
         VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password"]=md5($_POST['password'])."')";
         ($db->query($sql));
