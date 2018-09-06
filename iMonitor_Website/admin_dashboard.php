7e5d3048d8b6c0faf932fdc81a177778281b7d79
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-09-06 09:55:22 +0800

    edit admin_dashboard.php

--- iMonitor_Website/admin_dashboard.php
+++ iMonitor_Website/admin_dashboard.php
@@ -1,10 +1,10 @@
 <?php
-session_start();
+//session_start();
 
-if(!isset($_SESSION["username"])) {
-  header("Location: index.php");
-exit();
-}
+//if(!isset($_SESSION["username"])) {
+  //header("Location: index.php");
+//exit();
+//}
 ?>
 
 <!DOCTYPE html>
