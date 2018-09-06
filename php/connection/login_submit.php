e4e19fda4fa5b3624f666b013ffda59037917929
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-09-06 09:47:47 +0800

    edit login_submit.php

--- php/connection/login_submit.php
+++ php/connection/login_submit.php
@@ -3,34 +3,21 @@
 	
 	require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
   
-	$un = $_POST['username'];
-    $pw = $_POST['password'];
+	$un=$_POST['username'];
+    $pw=md5($_POST['password']);
 
-	
-    $stmt = $db->prepare("SELECT * from tbl_user WHERE username=? AND password=?");
-	$stmt->execute(array($un, $pw));
+    $stmt = $db->prepare("SELECT * from sys_user WHERE username=? AND password=?");
+    $stmt->execute(array($un, $pw));
     $row_count = $stmt->rowCount();
-   
-    //$sql = "SELECT * FROM tbl_user WHERE username = ?";
-    //$stmt = $db->prepare($sql);
-    //$result = $stmt->execute([$_POST['username']]);
-    //$un = $result->fetchAll();
-        //if (isset($un[0]) {
-        //if (password_verify($_POST['password'], $un[0]->password) {
-            //echo "login success";
-        //} else {
-            //echo "wrong password";
-        //}
-        //} 
-    if($row_count >0)
-        {
-            $_SESSION['username'] = $un;
-            header("Location: ../../iMonitor_Website/admin_dashboard.php");
-        }
-    else
-        { 
-            header("Location: ../../iMonitor_Website/index.php?msg=wrong");
-        }
+
+    if ($row_count > 0) {
+    $_SESSION["username"]=$un;
+        header("Location: main.php");
+    }
+
+    else {
+        header("Location: ../index_test.php?msg=wrong");
+    }
 ?>
                                      
                                      
