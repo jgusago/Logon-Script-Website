a915c42afd7c2c0d3e7e94c1062dacb3308a5304
orlancastelo <1040363@dt.com>
2018-09-10 04:00:54 +0800

    edit admin_users.php

--- php/connection/user_account_submit.php
+++ php/connection/user_account_submit.php
@@ -5,6 +5,6 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
          
         VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password"]=md5($_POST['password'])."')";
         ($db->query($sql));
-        header("Location: ../../iMonitor_Website/admin_users.php?msg=correct");
+        echo "<script>alert('User Account Save Successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
 
 ?>
