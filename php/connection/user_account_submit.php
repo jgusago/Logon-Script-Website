7484ca04e3a08f6894f4cde8617d8b90dc00c76c
orlancastelo <1040363@dt.com>
2018-09-10 06:54:42 +0800

    edit user_account_submit.php

--- php/connection/user_account_submit.php
+++ php/connection/user_account_submit.php
@@ -1,10 +1,24 @@
 <?php
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
+$username = $_POST['userid'];
+
+if(!isset($error)){ 
+        //no error
+        $stmt = $db->prepare("SELECT userid FROM tbl_user WHERE userid = :userid");
+        $stmt->execute(array($username));
+        $row_count = $stmt->rowCount();
+        
+        if ($row_count > 0){
+            echo "<script>alert('Username exist!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
+        }
+else
+    {
         $sql = "INSERT INTO tbl_user (userid, name, department, position, role, password)
          
         VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password"]=md5($_POST['password'])."')";
         ($db->query($sql));
         echo "<script>alert('User Account Save Successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
-
+    }
+}
 ?>
