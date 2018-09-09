fa2716b8c6443a3b30b5e46119fde926613dcef3
orlancastelo <1040363@dt.com>
2018-09-10 06:20:13 +0800

    edit admin_users.php

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -253,7 +253,7 @@ $ID=$_GET['id'];
 		?>
 
 <form action="user_edit_account_submit.php <?php echo '?id='.$id; ?>" method="POST">	
-	<div class="modal fade" id="#editUser" tabindex="-1" role="dialog" aria-labelledby="myModallabel" arial-hidden="true" style="margin-top:150px;">
+	<div class="modal fade" id="editUser" tabindex="-1" role="dialog" aria-labelledby="myModallabel" arial-hidden="true" style="margin-top:150px;">
 			<div class="modal-dialog modal-md" role="document">
 				<div class="modal-content">
 					<div class="modal-header" style="background-color:#16811430;"><b>Edit User Information</b>
