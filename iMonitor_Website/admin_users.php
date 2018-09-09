7c5baaca44da6a9f0cd58c9788c0bcb8929cb525
orlancastelo <1040363@dt.com>
2018-09-10 05:38:35 +0800

    edti admin_users.php

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -153,13 +153,6 @@ $ID=$_GET['id'];
             </div>
         </div>  
 
-	<?php
-		 $stmt = $db->query("SELECT * FROM tbl_user WHERE id='$ID'");
-		 $stmt->execute();
-		 for($i=0; $row = $stmt->fetch(); $i++){
-		 $id=$row['id'];
-	?>
-
 <form action="../php/connection/user_account_submit.php" method="POST">
     <div class="modal fade" id="addUser" tabindex="-1" role="dialog" aria-labelledby="myModallabel" arial-hidden="true" style="margin-top:150px;">
 			<div class="modal-dialog modal-md" role="document">
@@ -344,7 +337,6 @@ $ID=$_GET['id'];
 						</form>
 					</div>					
 </form>
-<?php } ?>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal" style="font-size:15px;">Close</button>
 					</div>			
