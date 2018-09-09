4f491d7fdc0c200b484a831ab69721c18b8cd704
orlancastelo <1040363@dt.com>
2018-09-10 05:32:07 +0800

    test

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -249,14 +249,15 @@ $ID=$_GET['id'];
 <form action="user_edit_account_submit.php <?php echo '?id='.$id; ?>" method="POST">	
 	<div class="modal fade" id="editUser" tabindex="-1" role="dialog" aria-labelledby="myModallabel" arial-hidden="true" style="margin-top:150px;">
 			<div class="modal-dialog modal-md" role="document">
-				<div class="modal-content">
 
-				 <?php
+ <?php
 		 $stmt = $db->query("SELECT * FROM tbl_user WHERE id='$ID'");
 		 $stmt->execute();
 		 for($i=0; $row = $stmt->fetch(); $i++){
 		 $id=$row['id'];
 	?>
+
+				<div class="modal-content">
 					<div class="modal-header" style="background-color:#16811430;"><b>Edit User Information</b>
 						<button type="button" class="close" class="btn btn-default" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 					</div>
