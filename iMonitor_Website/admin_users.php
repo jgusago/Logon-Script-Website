e139d0ea0dfa7b003e13e6da6b4a808f06522eca
orlancastelo <1040363@dt.com>
2018-09-10 04:25:43 +0800

    edit admin_users.php

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -245,7 +245,7 @@ $ID=$_GET['id'];
 
 		<!-- Modal Add User-->
 
-	<div class="modal fade" id="#editUser" tabindex="-1" role="dialog" aria-labelledby="myModallabel" arial-hidden="true" style="margin-top:150px;">
+	<div class="modal fade" id="editUser" tabindex="-1" role="dialog" aria-labelledby="myModallabel" arial-hidden="true" style="margin-top:150px;">
 			<div class="modal-dialog modal-md" role="document">
 				<div class="modal-content">
 					<div class="modal-header" style="background-color:#16811430;"><b>Edit User Information</b>
