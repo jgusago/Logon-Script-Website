500860798b1f8a49e50d422707e3f0eee514a956
orlancastelo <1040363@dt.com>
2018-09-10 06:18:58 +0800

    add file

---
+++ php/connection/user_edit_account_submit.php
@@ -0,0 +1,19 @@
+<?php 
+session_start();
+require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
+
+$get_id=$_REQUEST['id'];
+
+$userid= $_POST['userid'];
+$name= $_POST['name'];
+$department= $_POST['department'];
+$position= $_POST['position'];
+$role= $_POST['role'];
+$password= $_POST['password'];
+
+$stmt = $db->prepare("UPDATE tbl_user SET usernid='$userid', name='$name', department='$department', position='$position', role='$role', password='$password' WHERE id='$get_id'");
+$stmt->execute(array());
+$affected_rows = $stmt->rowCount();
+
+echo "<script>alert('Account updated successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
+?>
