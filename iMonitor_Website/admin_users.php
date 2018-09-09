b2c8f01abbbd5359b0796ba64fe49319963fd8bf
orlancastelo <1040363@dt.com>
2018-09-10 05:10:54 +0800

    edit admin_users.php

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -257,11 +257,11 @@ $ID=$_GET['id'];
 							<table class="modal-form">
 								<tr>
 									<td><b>ID Number:</b></td>
-									<td><input type="text" id="userid" name="userid" required placeholder="User ID"></td>
+									<td><input type="number" id="userid" name="userid" required placeholder="User ID"></td>
 								</tr>
 								<tr>
 									<td><b>Name:</b></td>
-									<td><input type="text" id="name" name="name" required placeholder="Name"></td>
+									<td><input type="text" pattern="[A-Za-z]*" id="name" name="name" required placeholder="Name" class="validate"></td>
 								</tr>
 								<tr>
 									<td><b>Department:</b></td>
