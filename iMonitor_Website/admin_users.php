28a5d35b981f32db4412aaf113c990e4a8afd529
orlancastelo <1040363@dt.com>
2018-09-11 06:45:21 +0800

    edit admin_users.php

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -4,10 +4,10 @@ error_reporting(0);
 session_start();
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 $ID=$_GET['id'];
-/*if(!isset($_SESSION["userid"])) {
-    header("Location: index.php");
-  exit();
-  }*/
+	if(!isset($_SESSION["userid"])) {
+    	header("Location: index.php");
+  	exit();
+  	}
 ?>
 
 <!DOCTYPE html>
