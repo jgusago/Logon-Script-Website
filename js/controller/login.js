8d92915c16a09a417ce5b9e6a797603db3a59df2
John Melvin Gusago <42765192+jgusago@users.noreply.github.com>
2018-09-10 16:46:19 +0800

    attemp 2

--- js/controller/login.js
+++ js/controller/login.js
@@ -1,5 +1,37 @@
 function confirmlogin(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    $.post("php/login_confirm.php",{username:username,password:password},function(data){
+
+    });
 
     return false;
 
 }
+
+/*
+
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var message = document.getElementById("login-message");
+
+    $.post("php/login_confirm.php",{username:username,password:password},function(data){
+        var dts = data.split(";");
+        switch(dts[0]){
+            case "success":
+                message.style.display = "block";
+            break;
+            case "failed":
+                message.style.display = "block";
+            break;
+            default:
+
+        }
+        //$("#sampledt").html(data+"status: "+status+"<br>active: "+active+"<br>passchange: "+passchange);
+        });
+
+        return false;
+
+        */
