891121c751e4335c98fa3091fc851af862247053
orlancastelo <1040363@dt.com>
2018-09-11 05:35:23 +0800

    edit test file

--- php/connection/user_edit_account_submit.php
+++ php/connection/user_edit_account_submit.php
@@ -13,7 +13,7 @@ $position= $_POST['position'];
 $role= $_POST['role'];
 $password= $_POST['password'];
 
-$stmt = $db->prepare("UPDATE tbl_user SET userid='$userid', name='$name', department='$department', position='$position', role='$role', password=md5($_POST['password']) WHERE id='$get_id'");
+$stmt = $db->prepare("UPDATE tbl_user SET userid='$userid', name='$name', department='$department', position='$position', role='$role', password=md5'$password' WHERE id='$get_id'");
 $stmt->execute(array());
 $affected_rows = $stmt->rowCount();
 
