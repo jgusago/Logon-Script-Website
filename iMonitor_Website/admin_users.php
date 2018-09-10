1b5275647ea8785c8977e3bab4f411f1bb3f54ea
orlancastelo <1040363@dt.com>
2018-09-11 04:20:32 +0800

    edit admin_users.php

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -257,10 +257,7 @@ $ID=$_GET['id'];
 							<table class="modal-form">
 								<tr>
 									<td><b>ID Number:</b></td>
-									<td><input type="number" id="userid" name="userid" value="<?php $stmt = $db->query("SELECT * FROM tbl_user WHERE id='$ID'");
-		 							$stmt->execute();
-		 							for($i=0; $row = $stmt->fetch(); $i++){
-		 							$id=$row['id']; ?> <?php echo $row['userid']; ?> <?php } ?>" required placeholder="User ID"></td>
+									<td><input type="number" id="userid" name="userid" value="<?php echo $row['userid']; ?> required placeholder="User ID"></td>
 								</tr>
 								<tr>
 									<td><b>Name:</b></td>
