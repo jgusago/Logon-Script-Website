f7fc457cebeeba2b3e4440362e34343a49f27a53
John Melvin Gusago <42765192+jgusago@users.noreply.github.com>
2018-09-10 16:14:49 +0800

    Password pattern

--- js/controller/login.js
+++ js/controller/login.js
@@ -2,49 +2,16 @@ function login(){
 
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+    var message = document.getElementById("login-message");
 
     $.post("php/login_confirm.php",{username:username,password:password},function(data){
         var dts = data.split(";");
         switch(dts[0]){
             case "success":
-            /*
-            var status = dts[1];
-            var active = dts[2];
-            var passchange = dts[3];
-            
-                    if(passchange == 0){
-                        if(status == 'admin'){
-                                window.location.assign("http://172.16.39.241/om/.admin.html");
-                            }
-                        else if(status == 'itStaff'){
-                                window.location.assign("http://172.16.39.241/om/.user.html");
-                            }
-                        else if (status == 'superadmin'){
-                            window.location.assign("http://172.16.39.241/om/.superadmin.html");
-                        }
-                        else{
-                            //do nothing
-                        }
-                        }
-                    else{
-                        $('#npd').removeAttr('hidden');
-                        $('#npcd').removeAttr('hidden');
-                        $('#confirmbtns').removeAttr('hidden');
-                        $('#loginbtn').attr('hidden',true);
-                        $('#newpassword').attr('required',true);
-                        $('#conpassword').attr('required',true);
-                        $("#loginform").removeAttr('onsubmit');
-                        $("#loginform").attr('onsubmit','return loginnewpass()');
-                        document.getElementById("username").disabled = true;
-                        document.getElementById("password").disabled = true;
-                        $("#notifylogin").html("<div class='alert alert-success'>You're Required to change your password before you continue</div>");
-                        }
-                        */
-                   
+                message.style.display = "block";
             break;
             case "failed":
-                $("#notifylogin").html("<div class='alert alert-danger'>"+dts[1]+"</div>");
-                
+                message.style.display = "block";
             break;
             default:
 
