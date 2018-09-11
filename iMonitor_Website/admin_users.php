1b5374e0844ed39f6607c57b5a3ce9705af366f9
orlancastelo <1040363@dt.com>
2018-09-12 06:18:26 +0800

    test

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -271,7 +271,7 @@ $ID=$_GET['id'];
 					</script>
 
 					<div class="modal-footer">
-						<input type="text" id="password2" name="password2"></td>
+						<input type="hidden" id="password2" name="password2"></td>
 						<button type="button" class="btn btn-default" data-dismiss="modal" style="font-size:15px;">Close</button>
 					</div>
 				</div>
