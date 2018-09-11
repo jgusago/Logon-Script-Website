c3cef00d2112a93b0edf4460efc1299669c2bbd4
John Melvin Gusago <42765192+jgusago@users.noreply.github.com>
2018-09-11 17:35:22 +0800

    Try PDO again

--- php/functions/grph.chrt/treeview/count.tree.view.content.php
+++ php/functions/grph.chrt/treeview/count.tree.view.content.php
@@ -1,5 +1,86 @@
 <?php
+$branch = $_POST["branch"];
+$parent = $_POST["grandparent"];
 
+require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
+
+$query = "SELECT *
+            FROM logonscript.tbl_tree
+            WHERE treename=:branch AND treeparent=:parent";
+
+$stmt = $db->prepare($query);
+$stmt->array(":branch"=>$branch,
+            ":parent"=>$parent);
+$stmt->execute();
+$rowcount = $stmt->rowCount();
+$result = $stmt->fetchAll();
+
+if($rowcount>0){
+
+    while($result as $row){
+        $filter = $row["treefilter"];
+        $secondquery = "SELECT *
+                        FROM logonscript.tbl_log
+                        WHERE :filtering AND building like :parent
+                        GROUP BY hostname
+                        ORDER BY hostname";
+
+        $sec_pdo = $db->prepare($secondquery);
+        $sec_pdo->array(":filtering"=>$filter
+                        ":parent"=>$parent);
+        
+        $sec_result = $sec_pdo->fetchAll();
+
+        if(!($sec_result as $row)){
+            //do nothing
+        }
+        else{
+            $count = 0;
+
+            while ($sec_result as $row){
+                $data = $row['hostname'];
+
+                if($count>0)
+                {
+                    echo '|';
+                }
+            
+                echo $data;
+            
+                $count++;
+
+            }
+        }
+
+        $query2 = mysqli_query($con,"select * from sky.tbl_log where $filter and building like '$parent' group by hostname order by hostname");
+/*
+        if(!$query2){
+            //do nothing
+        }
+
+        else{
+            $count = 0;
+            //print content
+            while ($row = mysqli_fetch_array($query2)){
+                $data = $row['hostname'];
+            
+                if($count>0)
+                {
+                    echo '|';
+                }
+            
+                echo $data;
+            
+                $count++;
+            }
+            
+        }*/
+
+    }
+
+
+}
+/*
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/connection.php";
 
 $branch = $_POST["branch"];
@@ -41,6 +122,6 @@ if(mysqli_num_rows($query)>0){
 
 mysqli_close($con);
 
-
+*/
 
 ?>
