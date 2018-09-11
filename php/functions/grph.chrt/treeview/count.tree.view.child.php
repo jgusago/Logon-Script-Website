6f1f18dea99f598ade8891fc5d70aad166d83353
John Melvin Gusago <42765192+jgusago@users.noreply.github.com>
2018-09-11 16:47:34 +0800

    Try PDO

--- php/functions/grph.chrt/treeview/count.tree.view.child.php
+++ php/functions/grph.chrt/treeview/count.tree.view.child.php
@@ -1,8 +1,36 @@
 <?php
-require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/connection.php";
-
 $bld = $_POST["branch"];
 
+//require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/connection.php";
+
+require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
+
+$stmt = $db->prepare("SELECT * FROM logonscript.tbl_tree WHERE treeparent='?' ORDER BY treename");
+$stmt->execute(array($bld));
+
+$count = 0;
+
+while  ($output = $stmt->fetch()){
+    $treename = $row['treename'];
+
+    if($count>0)
+    {
+        $output = $output.'|'.$treename;
+    }
+    else{
+    $output = $treename;
+    }
+    $count++;
+}
+
+if ($count>0){
+    echo $output;
+}
+else{
+    echo $count;
+}
+
+/*
 $query = mysqli_query($con,"select * from sky.tbl_treeview where treeparent='$bld' ORDER BY treename");
 
 $count = 0;
@@ -28,4 +56,6 @@ else{
 }
 
 mysqli_close($con);
+*/
+
 ?>
