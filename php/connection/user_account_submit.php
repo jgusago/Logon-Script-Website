2e50bf9ef75c30b8b6bc6c2683f7554c0bcf284e
orlancastelo <1040363@dt.com>
2018-09-12 05:51:43 +0800

    edit user_account_submit

--- php/connection/user_account_submit.php
+++ php/connection/user_account_submit.php
@@ -20,7 +20,7 @@ else
     {
         $sql = "INSERT INTO tbl_user (userid, name, department, position, role, password)
          
-        VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password"]=md5($_POST['password'])."')";
+        VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password2"]=md5($_POST['password'])."')";
         ($db->query($sql));
         echo "<script>alert('User Account Save Successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
     }
