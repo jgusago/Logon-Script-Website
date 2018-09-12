b1e4900da7e9b7831d9b169ef6ec8b0e91b5daca
orlancastelo <1040363@dt.com>
2018-09-13 06:54:53 +0800

    test

--- php/connection/login_submit.php
+++ php/connection/login_submit.php
@@ -1,7 +1,7 @@
 <?php
 //error_reporting(0);
 
-session_start();
+/*session_start();
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
 $un=$_POST['userid'];
@@ -18,22 +18,22 @@ if ($row_count > 0) {
 
 else {
     header("Location: ../../iMonitor_Website/index.php?msg=wrong");
-}
+}*/
 
 
-    /*$userid = $_POST['userid'];
-    $hash_password = $_POST['password2'];
+    $userid = $_POST['userid'];
+    //$hash_password = $_POST['password2'];
    
     $stmt = $db->prepare("SELECT userid, password FROM tbl_user WHERE userid=?");
     $stmt->execute([$userid]);
     $check_user=$stmt->fetch();
-    if(count($check_user)>0 && password_verify('rasmuslerdorf', $hash_password)) {
+    if(count($check_user)>0 && password_verify($_POST["password2"],$hashed_password)) {
       $_SESSION['userid']=$check_user['userid'];
                  header("Location: ../../iMonitor_Website/admin_dashboard.php");
     exit;          
     }
             else {
                 header("Location: ../../iMonitor_Website/index.php?msg=wrong");
-            }*/
+            }
 
 ?>
