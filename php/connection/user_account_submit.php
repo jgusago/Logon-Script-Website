d31f2ba14a9709a93d18d9878b8c4734f6d83683
orlancastelo <1040363@dt.com>
2018-09-13 04:04:25 +0800

    test

--- php/connection/user_account_submit.php
+++ php/connection/user_account_submit.php
@@ -26,7 +26,12 @@ else
 }*/
 
 if(isset($_POST['user_account_submit'])){
+    
     $hash_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $sql = "INSERT INTO sys_user (email, password, account_type) VALUES ('".$_POST['username']."', '$hash_password', '".$_POST['account_type']."')";
+    $sql = "INSERT INTO tbl_user (userid, name, department, position, role, password)
+         
+    VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password2"]=($_POST['password'])."')";
+    ($db->query($sql));
+    echo "<script>alert('User Account Save Successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
 }
 ?>
