bd9a8b4aa566f5006ec2c13a2cda579c9e6240c3
orlancastelo <1040363@dt.com>
2018-09-13 04:24:35 +0800

    test

--- php/connection/user_account_submit.php
+++ php/connection/user_account_submit.php
@@ -5,9 +5,9 @@ session_start();
 
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
-/*$username = $_POST['userid'];
+$username = $_POST['userid'];
 
-if(!isset($error)){ 
+/*if(!isset($error)){ 
         $stmt = $db->prepare("SELECT userid FROM tbl_user WHERE userid = :userid");
         $stmt->execute(array($username));
         $row_count = $stmt->rowCount();
@@ -25,10 +25,10 @@ else
     }
 }*/
     
-    //$hash_password = password_hash($_POST['password2'], PASSWORD_DEFAULT);
-    $sql = "INSERT INTO tbl_user (userid, name, department, position, role, password)
-         
-    VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password2"]=password_hash($_POST['password'])."')";
-    ($db->query($sql));
-    echo "<script>alert('User Account Save Successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
+    if(isset($_POST['register'])){
+        $hash_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $sql = "INSERT INTO sys_user (userid, password, role) VALUES ('".$_POST['userid']."', '$hash_password', '".$_POST['role']."')";
+        ($db->query($sql));
+        echo "<script>alert('User Account Save Successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
+    }
 ?>
