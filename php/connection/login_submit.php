0ada84ac6f35a4e5404ba0cf9d643f28576c742a
orlancastelo <1040363@dt.com>
2018-09-14 07:26:51 +0800

    test

--- php/connection/login_submit.php
+++ php/connection/login_submit.php
@@ -23,8 +23,6 @@ else {
 
     $userid = $_POST['userid'];
     $password = $_POST['password'];
-    $status = $_POST['status'];
-
    
     $stmt = $db->prepare("SELECT * FROM tbl_user WHERE userid=:userid LIMIT 1"); 
     $stmt->bindValue(':userid', $userid, PDO::PARAM_STR); 
