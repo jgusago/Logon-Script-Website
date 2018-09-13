ffc17e6180db1c9a3af15555ed20da0548163bb6
orlancastelo <1040363@dt.com>
2018-09-14 06:30:14 +0800

    test

--- php/connection/user_account_submit.php
+++ php/connection/user_account_submit.php
@@ -1,5 +1,5 @@
 <?php
-//error_reporting(0);
+error_reporting(0);
 
 session_start();
 
@@ -7,7 +7,7 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
 $userid = $_POST['userid'];
 
-/*if(!isset($error)){ 
+if(!isset($error)){ 
         $stmt = $db->prepare("SELECT userid FROM tbl_user WHERE userid = :userid");
         $stmt->execute(array($username));
         $row_count = $stmt->rowCount();
@@ -17,20 +17,18 @@ $userid = $_POST['userid'];
         }
 else
     {
-        $sql = "INSERT INTO tbl_user (userid, name, department, position, role, password)
-         
-        VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password2"]=md5($_POST['password'])."')";
+        $hashed_password = password_hash($_POST["password2"],PASSWORD_DEFAULT);
+ 
+        $sql = "INSERT INTO tbl_user (userid, name, department, position, role, status, password)
+             
+        VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["status"]."', '$hashed_password')";
         ($db->query($sql));
         echo "<script>alert('User Account Save Successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
     }
-}*/
-    
-    $hashed_password = password_hash($_POST["password2"],PASSWORD_DEFAULT);
- 
-    $sql = "INSERT INTO tbl_user (userid, name, department, position, role, status, password)
+}
+        /*$sql = "INSERT INTO tbl_user (userid, name, department, position, role, password)
          
-    VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["status"]."', '$hashed_password')";
-    ($db->query($sql));
-    echo "<script>alert('User Account Save Successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
-
+        VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["password2"]=md5($_POST['password'])."')";
+        ($db->query($sql));
+        echo "<script>alert('User Account Save Successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";*/
 ?>
