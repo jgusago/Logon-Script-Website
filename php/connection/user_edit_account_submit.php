e0ec1cd0a5a89d802126de4bfe277fce622b352a
orlancastelo <1040363@dt.com>
2018-09-14 05:46:21 +0800

    test

--- php/connection/user_edit_account_submit.php
+++ php/connection/user_edit_account_submit.php
@@ -12,10 +12,10 @@ $department= $_POST['department'];
 $position= $_POST['position'];
 $role= $_POST['role'];
 $status= $_POST['status'];
+$hashed_password = password_hash($_POST["password"],PASSWORD_DEFAULT);
 //$password= md5($_POST['password']);
-//$hashed_password = password_hash($_POST["password"],PASSWORD_DEFAULT);
 
-$stmt = $db->prepare("UPDATE tbl_user SET userid='$userid', name='$name', department='$department', position='$position', role='$role', status='$status' WHERE id='$get_id'");
+$stmt = $db->prepare("UPDATE tbl_user SET userid='$userid', name='$name', department='$department', position='$position', role='$role', status='$status', password='$hashed_password' WHERE id='$get_id'");
 $stmt->execute(array());
 $affected_rows = $stmt->rowCount();
 
