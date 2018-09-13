5b0d50b1ddd06927488b6c387aa4229977b5c770
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-09-13 13:56:50 +0800

    viewing.php(try)

---
+++ iMonitor_Website/viewing.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Our Custom CSS -->
+    <!-- <link rel="stylesheet" href="viewing.css"> -->
+    <link rel="stylesheet" href="style1.css">
+    <!-- <link rel="stylesheet" href="styleIndex.css"> -->
+</head>
+<body>
+    <div class="panel panel-default"></div>
+        <div class="panel-heading">
+            <h4 style="font-size:2em;">ITOMAU033022 | Camille</h4>
+        </div>
+        <div class="panel-body">
+        <table class="table table-bordered" style="margin-left:90px;">
+            <thead>
+                <tr style="padding:50px;">
+                    <th style="padding-bottom:15px;">Domain</th>
+                    <th style="padding-bottom:15px;">IP Address</th>
+                    <th style="padding-bottom:15px;">IP Address Modified</th>
+                    <th style="padding-bottom:15px;">Old IP Address</th>
+                    <th style="padding-bottom:15px;">Old IP Address Modified</th>
+                    <th style="padding-bottom:15px;">iMonitor Status</th>
+                    <th style="padding-bottom:15px;">Services Not Found</th>
+                    <th style="padding-bottom:15px;">SysSetting File</th>
+                    <th style="padding-bottom:15px;">Connection Status</th>
+                    <th style="padding-bottom:15px;">Branch</th>
+                    <th style="padding-bottom:15px;">Scan Time</th>
+                </tr>
+            </thead>
+            <tbody id="viewingTable">
+                <tr>
+                    <td style="padding-top:15px;">NN</td>
+                    <td style="padding-top:15px;">172.16.33.22</td>
+                    <td style="padding-top:15px;">Aug-08-2018 02:00</td>
+                    <td style="padding-top:15px;"></td>
+                    <td style="padding-top:15px;"></td>
+                    <td style="padding-top:15px;">Running</td>
+                    <td style="padding-top:15px;">Msysys.exe<br>Mskes.exe</td>
+                    <td style="padding-top:15px;">Found</td>
+                    <td style="padding-top:15px;">Established</td>
+                    <td style="padding-top:15px;">Marvin(IT)</td>
+                    <td style="padding-top:15px;">Aug-08-2018 02:00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="container">
+        <div class="panel panel-default"></div>
+            <div class="panel-heading">
+                <h4 style="font-size:2em;">ITOMAU033022 | Camille</h4>
+            </div>
+            <div class="panel-body">
+            <table class="table table-bordered" style="margin-left:90px;">
+                <thead>
+                    <tr style="padding:50px;">
+                        <th style="padding-bottom:15px;">Domain</th>
+                        <th style="padding-bottom:15px;">IP Address</th>
+                        <th style="padding-bottom:15px;">IP Address Modified</th>
+                        <th style="padding-bottom:15px;">Old IP Address</th>
+                        <th style="padding-bottom:15px;">Old IP Address Modified</th>
+                        <th style="padding-bottom:15px;">iMonitor Status</th>
+                        <th style="padding-bottom:15px;">Services Not Found</th>
+                        <th style="padding-bottom:15px;">SysSetting File</th>
+                        <th style="padding-bottom:15px;">Connection Status</th>
+                        <th style="padding-bottom:15px;">Branch</th>
+                        <th style="padding-bottom:15px;">Scan Time</th>
+                    </tr>
+                </thead>
+                <tbody id="viewingTable">
+                    <tr>
+                        <td style="padding-top:15px;">NN</td>
+                        <td style="padding-top:15px;">172.16.33.22</td>
+                        <td style="padding-top:15px;">Aug-08-2018 02:00</td>
+                        <td style="padding-top:15px;"></td>
+                        <td style="padding-top:15px;"></td>
+                        <td style="padding-top:15px;">Running</td>
+                        <td style="padding-top:15px;">Msysys.exe<br>Mskes.exe</td>
+                        <td style="padding-top:15px;">Found</td>
+                        <td style="padding-top:15px;">Established</td>
+                        <td style="padding-top:15px;">Marvin(IT)</td>
+                        <td style="padding-top:15px;">Aug-08-2018 02:00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
