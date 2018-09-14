5d4d4f47f90eedcce3633d4ea7091b24b0df204d
Melvin <jgusago@gmail.com>
2018-09-14 13:53:16 +0800

    edit users.php

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -124,16 +124,16 @@ $ID=$_GET['id'];
         </div>
         <div class="container-table" style="position:sticky;  overflow: hidden;">
             <div class="row" style="margin-top:200px">
-                <div class="panel panel-default" style="margin-top:-53px;">
+                <div class="panel panel-default" style="margin-top:0px;">
                     <div class="panel-heading">
                         <a href="#addUser" data-toggle="modal"><i class="glyphicon glyphicon-plus"></i><u>Add User</u></a>
-                    </div>
+					</div>
                 </div>
 			</div>
 		</div>
 
         <div class="container">
-            <table class="table table-bordered" style="position: fixed; margin-left:12px; margin-top:190px; margin-bottom:10px; width:87%">
+            <table class="table table-bordered" style="position: fixed; margin-left:12px; margin-top:210px; margin-bottom:10px; width:87%">
                 <thead>
                     <tr>
                         <th style="padding-bottom:15px;">No.</th>
