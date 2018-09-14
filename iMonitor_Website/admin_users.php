369fe855217f7372e4ced57ed7ae55d3a0a5c962
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-09-14 14:43:57 +0800

    commit

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -162,7 +162,8 @@ $ID=$_GET['id'];
 						<td><?php echo $row['position']; ?></td>
 						<td><?php echo $row['role']; ?></td>
 						<td><?php echo $row['status']; ?></td>
-						<td><a href="test_edit.php <?php echo '?id='.$id; ?>"><button class="btn btn-primary">Edit Record</button></td>
+						
+						<td><a href="#editUser <?php echo '?id='.$id; ?>" data-toggle="modal"><button class="btn btn-primary">Edit Record</button></td>
                         <!--<td><a href="#editUser" data-toggle="modal"><button class="btn btn-primary">Edit Record</button></a></td>-->
 					</tr>
 					<?php } ?>
