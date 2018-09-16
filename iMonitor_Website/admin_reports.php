bf6574dd019af8c711bf615010fdd22313630f4d
orlancastelo <1040363@dt.com>
2018-09-17 01:52:03 +0800

    edit admin_reports.php

--- iMonitor_Website/admin_reports.php
+++ iMonitor_Website/admin_reports.php
@@ -1,3 +1,15 @@
+<?php 
+//error_reporting(0);
+
+session_start();
+require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
+
+	if(!isset($_SESSION["userid"])) {
+    	header("Location: index.php");
+  	exit();
+  	}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
