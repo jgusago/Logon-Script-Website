f59cd66a01a83c87369928496e76edfdb5d95b77
orlancastelo <1040363@dt.com>
2018-09-17 01:35:22 +0800

    test

--- php/connection/test_function.php
+++ php/connection/test_function.php
@@ -1,8 +1,7 @@
 
-    <?php
+  <?php
 		$stmt = $db->query('SELECT * FROM tbl_user WHERE id='$id'');
 		$stmt->execute();
 		for($i=0; $row = $stmt->fetch(); $i++){
 		$id=$row['id'];
-		echo '?id='.$id;
 	?>
