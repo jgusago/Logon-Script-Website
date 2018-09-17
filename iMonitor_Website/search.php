524fe7604e7fc645f2d02d9fbf029c6595bf6458
orlancastelo <1040363@dt.com>
2018-09-18 06:12:11 +0800

    test

--- iMonitor_Website/search.php
+++ iMonitor_Website/search.php
@@ -6,7 +6,7 @@
     if (!$search) return;
 	$stmt = $db->prepare("SELECT DISTINCT user FROM `tbl_log` WHERE `user` LIKE '%$search%' ORDER BY `user` ASC");
     $stmt->execute(array());
-    //$row_count = $stmt->rowCount();
+    $row_count = $stmt->rowCount();
     while($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
         $user = $row['user'];
         echo "$name\n";
