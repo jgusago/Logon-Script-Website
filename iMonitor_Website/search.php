a4be2fea8099002ea0688c7fce1afe683f0eeef2
orlancastelo <1040363@dt.com>
2018-09-18 06:32:26 +0800

    test

--- iMonitor_Website/search.php
+++ iMonitor_Website/search.php
@@ -4,15 +4,15 @@
 session_start();
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
  
-$db_handle = new PDO();
 if(!empty($_POST["keyword"])) {
-$query ="SELECT * FROM user WHERE user like '" . $_POST["keyword"] . "%' ORDER BY user LIMIT 0,6";
-$result = $db_handle->runQuery($query);
-if(!empty($result)) {
+$sql = "SELECT * FROM user WHERE user like '" . $_POST["keyword"] . "%' ORDER BY user LIMIT 0,6";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+if(!empty($row)) {
 ?>
 <ul id="">
 <?php
-foreach($result as $country) {
+foreach($stmt as $row) {
 ?>
 <li onClick="selectUser('<?php echo $user["user"]; ?>');"><?php echo $user["user"]; ?></li>
 <?php } ?>
