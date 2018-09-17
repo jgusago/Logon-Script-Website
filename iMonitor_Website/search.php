bdf37f2ae3a067cfac0f299def6bf115f6302d11
orlancastelo <1040363@dt.com>
2018-09-18 06:27:59 +0800

    test

--- iMonitor_Website/search.php
+++ iMonitor_Website/search.php
@@ -4,7 +4,7 @@
 session_start();
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
  
-$db_handle = new DB();
+$db_handle = new PDO();
 if(!empty($_POST["keyword"])) {
 $query ="SELECT * FROM user WHERE user like '" . $_POST["keyword"] . "%' ORDER BY user LIMIT 0,6";
 $result = $db_handle->runQuery($query);
