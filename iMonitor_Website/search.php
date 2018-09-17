500f3830188b39a1ad6221d2fed1f9fad853ebff
orlancastelo <1040363@dt.com>
2018-09-18 05:21:46 +0800

    test

--- iMonitor_Website/search.php
+++ iMonitor_Website/search.php
@@ -1,15 +1,15 @@
 <?php
  session_start();
  require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
+ if(isset($_POST["query"]))   
  {  
       $output = '';  
-      $sql = "select user from tbl_log WHERE user LIKE '%".$_POST["query"]."%'";
-      $stmt = $db->prepare($sql);
+      $stmt = $pdo->query("select user from tbl_log WHERE user LIKE '%".$_POST["query"]."%'");
       $row_count = $stmt->rowCount();
      
       $output = '<ul class="list-unstyled">';  
       if($row_count > 0){
-        while($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
+        while ($row = $stmt->fetch()) {
            {  
                 $output .= '<li>'.$row["user"].'</li>';  
            }  
