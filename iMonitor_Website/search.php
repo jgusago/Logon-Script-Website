02baf1f3530a07c43a5b59c43a121a5e91d0722c
orlancastelo <1040363@dt.com>
2018-09-18 07:33:38 +0800

    test

--- iMonitor_Website/search.php
+++ iMonitor_Website/search.php
@@ -1,7 +1,7 @@
 
 <?php
 
-/*session_start();
+session_start();
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
  
 if(!empty($_POST["keyword"])) {
@@ -15,22 +15,9 @@ if(!empty($row_count)) {
 <?php
 foreach($stmt as $row) {
 ?>
-<li onClick="selectCountry('<?php echo $row["user"]; ?>');"><?php echo $row["user"]; ?></li>
+<li onClick="selectCountry('<?php echo $row["user"]; ?>');"></li>
+
 <?php } ?>
 
-<?php } }*/ 
-session_start();
-require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
- 
-if(!empty($_POST["keyword"])) {
-$sql = "SELECT DISTINCT user FROM tbl_log WHERE user like '" . $_POST["keyword"] . "%' ORDER BY user LIMIT 0,6";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-$row_count = $stmt->rowCount();
-if(!empty($row_count)) {
-foreach($stmt as $row) {
-echo $row["user"];
-        }
-    }
-}
+<?php } }
 ?>
