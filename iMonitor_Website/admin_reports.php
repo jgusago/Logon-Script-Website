527ddf985bd771dacf2e21beaed433edd47c89a2
orlancastelo <1040363@dt.com>
2018-09-19 04:53:14 +0800

    test

--- iMonitor_Website/admin_reports.php
+++ iMonitor_Website/admin_reports.php
@@ -182,10 +182,10 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
                 <div class="tab-pane fade in active" id="tab0default">
                     <div class="pane pane--table1" style="padding-right: 55px;">
                         <div class="col-md-3">
-                            <input type="text" name="from_date" class="form-control" placeholder="From Date" id="from_date"/> 
+                            <input type="text" id="from_date" name="from_date" class="form-control" placeholder="From Date"> 
                         </div>
                         <div class="col-md-3">
-                            <input type="text" name="to_date" class="form-control" placeholder="To Date" id="to_date"/> 
+                            <input type="text" id="to_date" name="to_date" class="form-control" placeholder="To Date"> 
                         </div>
                         <div class="col-md-3">
                             <select name="department" id="department" class="form-control">
