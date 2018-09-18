27bdba46c9d2f98dda327c9e5bc4413e8c375279
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-09-18 15:23:24 +0800

    another try (timer)

--- iMonitor_Website/admin_dashboard.php
+++ iMonitor_Website/admin_dashboard.php
@@ -164,16 +164,7 @@ exit();
     <!-- End of Sidebar -->
     
     <div class="container" style="margin-top:-900px;">
-        <div id="countdown">
-            <div id="tiles"></div>
-            <div class="labels">
-                <li>Days</li>
-                <li>Hours</li>
-                <li>Mins</li>
-                <li>Seconds</li>
-            </div>
-        </div>
-        <!-- <p id="demo"></p> -->
+        <p id="demo"></p>
     </div>
 
 	<script>
@@ -211,40 +202,11 @@ exit();
             input.value = input.value.replace(regex,"");   
      }  
 
-var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
-var days, hours, minutes, seconds; // variables for time units
-
-var countdown = document.getElementById("tiles"); // get tag element
-
-getCountdown();
-
-setInterval(function () { getCountdown(); }, 1000);
-
-function getCountdown(){
-
-	// find the amount of "seconds" between now and target
-	var current_date = new Date().getTime();
-	var seconds_left = (target_date - current_date) / 1000;
-
-	days = pad( parseInt(seconds_left / 86400) );
-	seconds_left = seconds_left % 86400;
-		 
-	hours = pad( parseInt(seconds_left / 3600) );
-	seconds_left = seconds_left % 3600;
-		  
-	minutes = pad( parseInt(seconds_left / 60) );
-	seconds = pad( parseInt( seconds_left % 60 ) );
-
-	// format countdown string + set tag value
-	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
+     var myVar = setInterval(myTimer ,1000);
+function myTimer() {
+    var d = new Date();
+    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
 }
-
-function pad(n) {
-	return (n < 10 ? '0' : '') + n;
-}
-
-
-
 	
 </body>
 </html>
