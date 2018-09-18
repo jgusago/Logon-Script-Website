8bf35436a3ccb29a2e6130867df43fcc618f4bf2
orlancastelo <1040363@dt.com>
2018-09-19 04:10:09 +0800

    test

--- iMonitor_Website/ajax.js
+++ iMonitor_Website/ajax.js
@@ -1,24 +1,26 @@
 $(document).ready(function(){
-	$('#user').textbox();
-	$('#btn_search').on('click', function(){	
-		if($('#user').val() == ""){
-			alert("Please enter something on the text field");
-		}else
-		{
-			$user = $('#user').val();
-			$('#load_data').empty();
+	$('#from_date').datepicker();
+	$('#to_date').datepicker();
+	$('#btn_filter').on('click', function(){	
+		if($('#from_date').val() == "" || $('#to_date').val() == ""){
+			alert("Please Select Date");
+		}else{
+			$from_date = $('#from_date').val();
+			$to_date = $('#to_date').val();
+			$('#get_data').empty();
 			$loader = $('<tr ><td colspan = "4"><center>Searching....</center></td></tr>');
-			$loader.appendTo('#load_data');
+			$loader.appendTo('#get_data');
 			setTimeout(function(){
 				$loader.remove();
 				$.ajax({
-					url: 'get_data_byname.php',
+					url: 'get_data.php',
 					type: 'POST',
 					data: {
-						user: $user
+						from_date: $from_date,
+						to_date: $to_date
 					},
 					success: function(res){
-						$('#load_data').html(res);
+						$('#get_data').html(res);
 					}
 				});
 			}, 3000);
