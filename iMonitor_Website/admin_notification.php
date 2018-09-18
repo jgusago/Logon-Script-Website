03c0dfdef61c572d0cd1389e66c1d4f032afb253
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-09-18 19:27:11 +0800

    try

--- iMonitor_Website/admin_notification.php
+++ iMonitor_Website/admin_notification.php
@@ -1,3 +1,39 @@
+<script type="text/javascript">
+var IDLE_TIMEOUT = 600; //seconds
+var _idleSecondsCounter = 0;
+document.onclick = function() {
+_idleSecondsCounter = 0;
+};
+document.onmousemove = function() {
+_idleSecondsCounter = 0;
+};
+document.onkeypress = function() {
+_idleSecondsCounter = 0;
+};
+window.setInterval(CheckIdleTime, 1000);
+function CheckIdleTime() {
+_idleSecondsCounter++;
+var oPanel = document.getElementById("SecondsUntilExpire");
+if (oPanel)
+oPanel.innerHTML = (IDLE_TIMEOUT - _idleSecondsCounter) + "";
+if (_idleSecondsCounter >= IDLE_TIMEOUT) {
+//alert("Time expired!");
+document.location.href = "../php/connection/logout.php";
+}
+}
+</script>
+
+<?php
+session_start();
+
+require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
+
+if(!isset($_SESSION["userid"])) {
+  header("Location: index.php");
+exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
