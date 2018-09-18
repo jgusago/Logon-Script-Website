bb38460820891160e8e705f602f0a7f492359048
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-09-18 16:18:52 +0800

    edit computer logs

--- iMonitor_Website/admin_reports.php
+++ iMonitor_Website/admin_reports.php
@@ -215,14 +215,17 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
                                 </thead>
                                 <tbody id = "load_data">
                                     <?php
-		            	               $sql = "select log_no, user, domain_name, hostname, ip_address, ip_date_modified, old_ip_address, old_ip_modified, iMonitor_Status, services, sysSetting_File, serverIP, connection_status, branch, scan_time from tbl_log ORDER BY log_no ASC";
+                                       $sql = "select user, domain_name, hostname, ip_address, ip_date_modified, old_ip_address, old_ip_modified, 
+                                       iMonitor_Status, services, sysSetting_File, serverIP, connection_status, branch, scan_time from tbl_log 
+                                       WHERE user != 'Administrator' ";
                                        $stmt = $db->prepare($sql);
                                        $stmt->execute();
+                                       $count = 1;
                                        foreach($stmt as $row) {
                                        //while($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
 	            	                 ?>
                                 <tr> 
-                                    <td><?php echo $row['log_no']; ?></td>
+                                    <td><?php echo $count++ ?></td>
                                     <td><?php echo $row['user']; ?></td>
                                     <td><?php echo $row['domain_name']; ?></td>
                                     <td><?php echo $row['hostname']; ?></td>
