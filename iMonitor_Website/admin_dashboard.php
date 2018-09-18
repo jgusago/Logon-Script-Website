a72c57ee175a07a5c1fb6572b1b70d52452ac5da
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-09-18 14:30:58 +0800

    timer (try)

--- iMonitor_Website/admin_dashboard.php
+++ iMonitor_Website/admin_dashboard.php
@@ -161,9 +161,13 @@ exit();
 	   		</ul>
 		</nav>
 	</div>               
-	<!-- End of Sidebar -->
+    <!-- End of Sidebar -->
+    
+    <div class="container">
+        <p id="demo"></p>
+    </div>
 
-	<script type="text/javascript">
+	<script>
 
         $(document).ready(function(){
             $(".dropdown").hover(            
@@ -196,7 +200,35 @@ exit();
             function lettersOnly(input) {
             var regex = /[^a-z]/gi;
             input.value = input.value.replace(regex,"");   
-     }      
+     }  
+
+        var countDownDate = new Date("Sept 18 , 2018 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000); 
 
 </script>
 
