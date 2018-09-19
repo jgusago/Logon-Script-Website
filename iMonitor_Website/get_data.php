aa87525098bab625d1ee585111cb6dcc0df6a81b
orlancastelo <1040363@dt.com>
2018-09-20 04:10:04 +0800

    save

--- iMonitor_Website/get_data.php
+++ iMonitor_Website/get_data.php
@@ -5,7 +5,7 @@ $to_date = date("MMM-dd-yyyy hh:mm", strtotime($_POST['to_date']));
 session_start();
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 	
-$sql = "select log_no, user, domain_name, hostname, ip_address, ip_date_modified, old_ip_address, old_ip_modified, iMonitor_Status, services, sysSetting_File, serverIP, connection_status, branch, scan_time from tbl_log WHERE scan_time BETWEEN 'Sep-11-2018 11:10' AND 'Sep-11-2018 11:10'";
+$sql = "select log_no, user, domain_name, hostname, ip_address, ip_date_modified, old_ip_address, old_ip_modified, iMonitor_Status, services, sysSetting_File, serverIP, connection_status, branch, scan_time from tbl_log WHERE scan_time BETWEEN '$from_date' AND '$to_date'";
 $stmt = $db->prepare($sql);
 $row_count = $stmt->rowCount();
 if($row_count > 0){
