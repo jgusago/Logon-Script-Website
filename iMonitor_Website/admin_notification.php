894e122afc02754720d73ad03daf480136fecaa8
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-09-19 09:10:55 +0800

    margin-left

--- iMonitor_Website/admin_notification.php
+++ iMonitor_Website/admin_notification.php
@@ -182,7 +182,7 @@ exit();
 	   		</ul>
         </nav>
         <div class="container" style="width:2000px;">
-            <div class="panel" style="margin-top: 60px; background-color: #e3e5e3eb;">
+            <div class="panel" style="margin-top: 60px; background-color: #e3e5e3eb; margin-left:-15px;">
                 <div class="col-lg-12">
                     <div class="panel with-nav-tabs pane-default" style="margin-left: -15px;">
                         <div class="panel-heading">
