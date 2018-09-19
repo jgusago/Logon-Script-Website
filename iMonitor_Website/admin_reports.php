74942ddf63a364b2a5dddc7fb1079bb4fa6711ae
orlancastelo <1040363@dt.com>
2018-09-20 07:27:57 +0800

    test

--- iMonitor_Website/admin_reports.php
+++ iMonitor_Website/admin_reports.php
@@ -1,3 +1,28 @@
+<script type="text/javascript">
+var IDLE_TIMEOUT = 600; //seconds
+var _idleSecondsCounter = 0;
+document.onclick = function() {
+_idleSecondsCounter = 0;
+};
+document.onmousemove = function() {
+_idleSecondsCounter = 0;
+};
+document.onkeypress = function() {
+_idleSecondsCounter = 0;
+};
+window.setInterval(CheckIdleTime, 1000);
+function CheckIdleTime() {
+_idleSecondsCounter++;
+var oPanel = document.getElementById("SecondsUntilExpire");
+if (oPanel)
+oPanel.innerHTML = (IDLE_TIMEOUT - _idleSecondsCounter) + "";
+if (_idleSecondsCounter >= IDLE_TIMEOUT) {
+//alert("Time expired!");
+document.location.href = "../php/connection/logout.php";
+}
+}
+</script>
+
 <?php 
 //error_reporting(0);
 
