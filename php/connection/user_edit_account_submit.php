05605ddab14c9ac94cb382bbdf4a156989a2f18c
orlancastelo <1040363@dt.com>
2018-09-21 04:55:30 +0800

    test

--- php/connection/user_edit_account_submit.php
+++ php/connection/user_edit_account_submit.php
@@ -15,8 +15,8 @@ $status= $_POST['status'];
 $hashed_password = password_hash($_POST["password2"],PASSWORD_DEFAULT);
 //$password= md5($_POST['password']);
 
-/*if($_POST['userid'] !== $_POST['userid2']) {
-if(!isset($error)){
+if($_POST['userid'] !== $_POST['userid2']) {
+//if(!isset($error)){
     $stmt = $db->prepare("SELECT userid FROM tbl_user WHERE userid = :userid");
     $stmt->execute(array($userid));
     $row_count = $stmt->rowCount();
@@ -30,22 +30,5 @@ else {
         $affected_rows = $stmt->rowCount();
         echo "<script>alert('Account updated successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
         }
-    }
-}*/
-
-if($_POST['userid'] == $_POST['userid2']) {
-        $stmt = $db->prepare("UPDATE tbl_user SET userid='$userid', name='$name', department='$department', position='$position', role='$role', status='$status', password='$hashed_password' WHERE id='$get_id'");
-        $stmt->execute(array());
-        $affected_rows = $stmt->rowCount();
-        echo "<script>alert('Account updated successfully!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
-    }
-    else {
-        $stmt = $db->prepare("SELECT userid FROM tbl_user WHERE userid = :userid");
-        $stmt->execute(array($userid));
-        $row_count = $stmt->rowCount();
-    
-        if ($row_count > 0){
-            echo "<script>alert('Username exist!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
-        }
-    }        
+    }     
 ?>
