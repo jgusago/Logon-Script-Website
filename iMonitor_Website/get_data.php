a3f9e76ae7c877fd89431ac845018f8b5557c096
orlancastelo <1040363@dt.com>
2018-09-21 06:04:14 +0800

    test

--- iMonitor_Website/get_data.php
+++ iMonitor_Website/get_data.php
@@ -1,13 +1,15 @@
 <?php
 //$from_date = date("m/d/Y", strtotime($_POST['from_date']));
 //$to_date = date("m/d/Y", strtotime($_POST['to_date']));
-$from_date = DATE_FORMAT("%M-%d-%Y", date($_POST['from_date']));
-$from_date = DATE_FORMAT("%M-%d-%Y", date($_POST['from_date']));
+$from_date = new DateTime($POST['from_date']);
+$format_fromdate = date_format($from_date, 'M-d-Y');
+$to_date = new DateTime($POST['to_date']);
+$format_todate = date_format($to_date, 'M-d-Y');
 
 session_start();
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 	
-$sql = "select log_no, user, domain_name, hostname, ip_address, ip_date_modified, old_ip_address, old_ip_modified, iMonitor_Status, services, sysSetting_File, serverIP, connection_status, branch, scan_time from tbl_log WHERE scan_time BETWEEN '$from_date' AND '$to_date'";
+$sql = "select log_no, user, domain_name, hostname, ip_address, ip_date_modified, old_ip_address, old_ip_modified, iMonitor_Status, services, sysSetting_File, serverIP, connection_status, branch, scan_time from tbl_log WHERE scan_time BETWEEN '$format_fromdate' AND '$format_todate'";
 $stmt = $db->prepare($sql);
 $count = 1; 
 $row_count = $stmt->rowCount();
@@ -30,7 +32,7 @@ if($row_count > 0){
         <td><?php echo $row['serverIP']; ?></td>
         <td><?php echo $row['connection_status']?></td>
         <td><?php echo $row['branch']; ?></td>
-        <td><?php echo date("m/d/Y", strtotime($row['scan_time'])) ?></td>
+        <td><?php echo date("M-d-Y h:m", strtotime($row['scan_time'])) ?></td>
 	</tr>
 	<?php
 	}
