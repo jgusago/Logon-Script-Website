5ad7f73e6f597f46d419341e60f6114e762fc81c
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-09-20 12:32:46 +0800

    col-md 6

--- iMonitor_Website/admin_reports.php
+++ iMonitor_Website/admin_reports.php
@@ -214,7 +214,7 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
                         <div class="col-md-3">
                             <input type="text" id="to_date" name="to_date" class="form-control" placeholder="To Date"> 
                         </div> -->
-                        <div class="col-md-6">
+                        <div class="col-md-6" style="padding-top:15px;">
                             <select name="department" id="department" class="form-control">
                                 <option value="" selected>--All department--</option>
                                 <option value="Marvin 5th">Marvin 5th</option>
@@ -222,7 +222,7 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
                                 <option value="AT">AT</option>
                             </select> 
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6" style="padding-top:15px;">
                             <select name="dub_dept" id="sub_dept" class="form-control">
                                 <option value="" selected>--All sub department</option>
                                 <option value="OM">IT-OM</option>
@@ -242,55 +242,56 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
                         </div>
                         <div style="clear:both; padding:15px;">
                             <table class="table table-bordered" id="comp_logs">
-                            <thead>
-                                <tr>
-                                    <th>No.</th>
-                                    <th>User</th>
-                                    <th>Computer Name</th>
-                                    <th>Domain</th>
-                                    <th>IP Address</th>
-                                    <th>Date Modified</th>
-                                    <th>Old IP Address</th>
-                                    <th>Date Modified</th>
-                                    <th>iMonitor Status</th>
-                                    <th>Services Not Found</th>
-                                    <th>SysSetting File</th>
-                                    <th>Server IP</th>
-                                    <th>Connection Status</th>
-                                    <th>Branch</th>
-                                    <th>Scan Time</th>
-                                </tr>
+                                <thead>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>User</th>
+                                        <th>Computer Name</th>
+                                        <th>Domain</th>
+                                        <th>IP Address</th>
+                                        <th>Date Modified</th>
+                                        <th>Old IP Address</th>
+                                        <th>Date Modified</th>
+                                        <th>iMonitor Status</th>
+                                        <th>Services Not Found</th>
+                                        <th>SysSetting File</th>
+                                        <th>Server IP</th>
+                                        <th>Connection Status</th>
+                                        <th>Branch</th>
+                                        <th>Scan Time</th>
+                                    </tr>
                                 </thead>
                                 <tbody id = "load_data">
                                     <?php
-                                       $sql = "select user, domain_name, hostname, ip_address, ip_date_modified, old_ip_address, old_ip_modified, 
-                                       iMonitor_Status, services, sysSetting_File, serverIP, connection_status, branch, scan_time from tbl_log 
-                                       WHERE user != 'Administrator' ";
-                                       $stmt = $db->prepare($sql);
-                                       $stmt->execute();
-                                       $count = 1;
-                                       foreach($stmt as $row) {
-                                       //while($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
-	            	                 ?>
-                                <tr> 
-                                    <td><?php echo $count++ ?></td>
-                                    <td><?php echo $row['user']; ?></td>
-                                    <td><?php echo $row['hostname']; ?></td>
-                                    <td><?php echo $row['domain_name']; ?></td>
-                                    <td><?php echo $row['ip_address']; ?></td>
-                                    <td><?php echo $row['ip_date_modified']; ?></td>
-                                    <td><?php echo $row['old_ip_address']; ?></td>
-                                    <td><?php echo $row['old_ip_modified']; ?></td>
-                                    <td><?php echo $row['iMonitor_Status']; ?></td>
-                                    <td><?php echo $row['services']; ?></td>
-                                    <td><?php echo $row['sysSetting_File']; ?></td>
-                                    <td><?php echo $row['serverIP']; ?></td>
-                                    <td><?php echo $row['connection_status']?></td>
-                                    <td><?php echo $row['branch']; ?></td>
-                                    <!-- <td>php echo date("MMM-dd-yyyy hh:mm", strtotime($row['scan_time'])) ?></td> -->
-                                    <td><?php echo $row['scan_time'] ?></td
-                                </tr>
-                                <?php } ?>
+                                        $sql = "select user, domain_name, hostname, ip_address, ip_date_modified, old_ip_address, old_ip_modified, 
+                                        iMonitor_Status, services, sysSetting_File, serverIP, connection_status, branch, scan_time from tbl_log 
+                                        WHERE user != 'Administrator' ";
+                                        $stmt = $db->prepare($sql);
+                                        $stmt->execute();
+                                        $count = 1;
+                                        foreach($stmt as $row) 
+                                        {
+                                        //while($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
+                                    ?>
+                                    <tr> 
+                                        <td><?php echo $count++ ?></td>
+                                        <td><?php echo $row['user']; ?></td>
+                                        <td><?php echo $row['hostname']; ?></td>
+                                        <td><?php echo $row['domain_name']; ?></td>
+                                        <td><?php echo $row['ip_address']; ?></td>
+                                        <td><?php echo $row['ip_date_modified']; ?></td>
+                                        <td><?php echo $row['old_ip_address']; ?></td>
+                                        <td><?php echo $row['old_ip_modified']; ?></td>
+                                        <td><?php echo $row['iMonitor_Status']; ?></td>
+                                        <td><?php echo $row['services']; ?></td>
+                                        <td><?php echo $row['sysSetting_File']; ?></td>
+                                        <td><?php echo $row['serverIP']; ?></td>
+                                        <td><?php echo $row['connection_status']?></td>
+                                        <td><?php echo $row['branch']; ?></td>
+                                        <!-- <td>php echo date("MMM-dd-yyyy hh:mm", strtotime($row['scan_time'])) ?></td> -->
+                                        <td><?php echo $row['scan_time'] ?></td
+                                    </tr>
+                                    <?php } ?>
                                 </tbody>
                             </table>
                         </div>
@@ -304,7 +305,7 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
                         <div class="col-md-3">
                             <input type="text" id="to_date2" name="to_date2" class="form-control" placeholder="To Date"> 
                         </div> -->
-                        <div class="col-md-6">
+                        <div class="col-md-6" style="padding-top:15px;">
                             <select name="department" id="department" class="form-control">
                                 <option value="" selected>--All department--</option>
                                 <option value="Marvin 5th">Marvin 5th</option>
@@ -312,7 +313,7 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
                                 <option value="AT">AT</option>
                             </select> 
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6" style="padding-top:15px;">
                             <select name="dub_dept" id="sub_dept" class="form-control">
                                 <option value="" selected>--All sub department</option>
                                 <option value="OM">IT-OM</option>
