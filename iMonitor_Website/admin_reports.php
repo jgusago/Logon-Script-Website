fac15be70dcf700b247f59c5bf85764972e32f3a
orlancastelo <1040363@dt.com>
2018-09-21 05:23:30 +0800

    test

--- iMonitor_Website/admin_reports.php
+++ iMonitor_Website/admin_reports.php
@@ -288,7 +288,7 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
                                         <td><?php echo $row['serverIP']; ?></td>
                                         <td><?php echo $row['connection_status']?></td>
                                         <td><?php echo $row['branch']; ?></td>
-                                        <td><?php echo date("M-d-Y h:m", strtotime($row['scan_time']))?></td>
+                                        <td><?php echo date("MM-d-Y", strtotime($row['scan_time']))?></td>
                                     </tr>
                                     <?php } ?>
                                 </tbody>
