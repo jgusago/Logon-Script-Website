7c9c2a7fbe9cb93570e8e120e0d3313e90b6a3ec
orlancastelo <1040363@dt.com>
2018-09-25 04:33:38 +0800

    test

--- iMonitor_Website/useredittest.php
+++ iMonitor_Website/useredittest.php
@@ -44,7 +44,6 @@ $ID=$_GET['id'];
 	<meta http-equiv="refresh" content="300">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- Our Custom CSS -->
