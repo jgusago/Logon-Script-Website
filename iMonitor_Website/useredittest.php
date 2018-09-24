91376caa94c84d242a0603c82ad41e449bde578c
orlancastelo <1040363@dt.com>
2018-09-25 04:30:25 +0800

    test

--- iMonitor_Website/useredittest.php
+++ iMonitor_Website/useredittest.php
@@ -245,26 +245,71 @@ $ID=$_GET['id'];
             	</tbody>
             </table>
 	
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
+  <!-- Modal New Edit-->
+  	<div class="modal fade" id="myModal" role="dialog">
+    	<div class="modal-dialog">
     
       <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
+      	<div class="modal-content">
+        	<div class="modal-header">
+          	<button type="button" class="close" data-dismiss="modal">&times;</button>
+        	<h4 class="modal-title">Modal Header</h4>
         </div>
         <div class="modal-body">
-          <p>Some text in the modal.</p>
+		<form style="padding:10px;">
+			<table class="modal-form">
+				<tr>
+					<td><b>ID Number:</b></td>
+					<td><input type="text" id="userid" value=<?php echo $row['userid']?> name="userid" required placeholder="User ID"></td>
+				</tr>
+				<tr>
+					<td><b>Name:</b></td>
+					<td><input type="text" id="name" name="name" value=<?php echo $row['name']?>  required  class="validate"></td>
+				</tr>
+				<tr>
+					<td><b>Department:</b></td>
+					<td class="dropdown-dept">
+						<select name="department">
+							<option value=<?php echo $row['userid']?>> <?php echo $row['department']?> </option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td><b>Position:</b></td>
+					<td class="dropdown-dept">
+						<select name="position">
+							<option value=<?php echo $row['userid']?> > <?php echo $row['position']?> </option>
+						</select>
+				</tr>
+				<tr>
+					<td><b>Role:</b></td>
+					<td class="dropdown-role"> 
+						<select name="role">
+							<option value=<?php echo $row['userid']?>> <?php echo $row['role']?> </option>
+						</select>
+					</td>
+                </tr>
+                <tr class="reset-password">
+                    <td><b>Password:</b></td>
+                    <td><input id="default-pass" type="password" value=<?php echo $row['password']?>></td> 
+                    <td>
+            	<div>
+                	<label class="checkbox-inline"><input type="checkbox" id="myCheck" onclick="resetPass()" style=" margin-left: -170px;">Reset password</label>
+            	</div>
+                    </td>
+                </tr>
+				<tr>
+					<td></td>
+					<td><button class="btn btn-primary">Update</button></td>
+				</tr>
+			</table>
+		</form>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         </div>
-      </div>
-      
+      	</div>    
     </div>
-  
 </div>
 		</div> 
 		<!-- End of User Table -->
