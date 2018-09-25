40ad802fe68bfce8c757260c848dfc04e6b5e529
orlancastelo <1040363@dt.com>
2018-09-26 04:12:43 +0800

    test

--- iMonitor_Website/useredittest.php
+++ iMonitor_Website/useredittest.php
@@ -250,19 +250,65 @@ $ID=$_GET['id'];
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
+          <h4 class="modal-title">Edit User Information</h4>
         </div>
         <div class="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
+		<form style="padding:10px;">
+			<table class="modal-form">
+				<tr>
+					<td><b>ID Number:</b></td>
+					<td><input type="text" id="userid" value="" name="userid" required placeholder="User ID"></td>
+				</tr>
+				<tr>
+					<td><b>Name:</b></td>
+					<td><input type="text" id="name" name="name" value=""  required  class="validate"></td>
+				</tr>
+				<tr>
+					<td><b>Department:</b></td>
+					<td class="dropdown-dept">
+						<select name="department">
+						<option value=""></option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td><b>Position:</b></td>
+					<td class="dropdown-dept">
+						<select name="position">
+							<option value=""></option>
+						</select>
+				</tr>
+				<tr>
+					<td><b>Role:</b></td>
+					<td class="dropdown-role"> 
+						<select name="role">
+							<option value=""> </option>
+						</select>
+					</td>
+                </tr>
+                <tr class="reset-password">
+                    <td><b>Password:</b></td>
+                    <td><input id="default-pass" type="password" value=""></td> 
+                    <td>
+                        <div>
+                            <label class="checkbox-inline"><input type="checkbox" id="myCheck" onclick="resetPass()" style=" margin-left: -170px;">Reset password</label>
+                        </div>
+                    </td>
+                </tr>
+				<tr>
+					<td></td>
+					<td><button class="btn btn-primary">Update</button></td>
+				</tr>
+			</table>
+		</form>
+    </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         </div>
       </div>
     </div>
   </div>
-
-		</div>	
+</div>	
 		<!-- End of User Table -->
 
 <!-- Add User Modal -->
