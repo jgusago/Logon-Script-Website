c384209778d36f149db4d7df357e65bca277a7f7
orlancastelo <1040363@dt.com>
2018-09-26 07:52:49 +0800

    test

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -233,7 +233,9 @@ $ID=$_GET['id'];
 						<td><?php echo $row['role']; ?></td>
 						<td><?php echo $row['status']; ?></td>
 						<!--<td><a href="test_edit.php ?php //echo '?id='.$id; ?>" data-toggle="modal"><button class="btn btn-primary">Edit Record</button></a></td>-->
-						<td><a href="#edit" data-toggle="modal"><button class="btn btn-primary">Edit Records</button></a></td>
+						<td>
+						<a href="#edit_<?php echo $row['id']; ?>" class="btn btn-success btn-sm" data-toggle="modal"><span class="glyphicon glyphicon-edit"></span> Edit</a>
+						</td>
 						<?php require "{$_SERVER['DOCUMENT_ROOT']}/iMonitor_Website/test_modal.php"; ?>
 					</tr>
 					<?php } ?>
