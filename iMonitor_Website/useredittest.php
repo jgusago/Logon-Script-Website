417dc5819b903bdef50877bc883ab80883231bd1
orlancastelo <1040363@dt.com>
2018-09-26 09:11:19 +0800

    test

--- iMonitor_Website/useredittest.php
+++ iMonitor_Website/useredittest.php
@@ -234,9 +234,7 @@ $ID=$_GET['id'];
 						<td><?php echo $row['role']; ?></td>
 						<td><?php echo $row['status']; ?></td>
 						<td><a href="#edit_<?php echo '?id='.$id; ?>" data-toggle="modal"><button class="btn btn-primary">Edit Record</button></a></td>
-						<div>
 						<td><?php include('test_modal.php'); ?></td>
-						</div>
 					</tr>
 					<?php } ?>
             	</tbody>
