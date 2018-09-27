c27274a0a1fc29a06a5aeb0e6c09c1904b9ba5ca
orlancastelo <1040363@dt.com>
2018-09-28 06:29:06 +0800

    test

--- iMonitor_Website/test_edit.php
+++ iMonitor_Website/test_edit.php
@@ -33,14 +33,10 @@ $ID=$_GET['id'];
 <!DOCTYPE html>
 <html>
   <head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="refresh" content="300">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<meta http-equiv="refresh" content="300; charset=UTF-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <title>Imonitor Website</title>
     <link href="test.css" rel="stylesheet" type="text/css" />
   
@@ -105,7 +101,7 @@ $ID=$_GET['id'];
         <label for="password">Password:</label><br>
         <input type="text" id="password" name="password" value="Aa123456" disabled><br>
         <label for="department">Department:</label><br>
-            			<select id="department" name="department">
+            			<select id="department" name="department" value="<?php echo $row['department']; ?>">
 
                           				<?php     
               								$sql = "select branch_name from tbl_department";
