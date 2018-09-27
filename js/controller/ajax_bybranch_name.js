601798882ca690edd85936f2db282971d5fa9e17
orlancastelo <1040363@dt.com>
2018-09-28 04:25:19 +0800

    test

--- js/controller/ajax_bybranch_name.js
+++ js/controller/ajax_bybranch_name.js
@@ -1,13 +1,20 @@
-$(document).on('change','#department',function(){
-	var val = $(this).val();
-	$.ajax({
-		  url: 'get_sub_department.php',
-		  data: {distrito:val},
-		  type: 'POST',
-		  dataType: 'html',
-		  success: function(result){
-			   $('#position').html();  
-			   $('#position').html(result); 
-		  }
-	 });
+$(document).ready(function(){
+	$('#department').text();
+		$('#department').on('change', function(){
+		if($('#department').val() == ""){
+			alert("Please enter name");
+		}else{
+			$department = $('#department').val();
+				$.ajax({
+					url: 'get_sub_department.php',
+					type: 'POST',
+					data: {
+						department: $department
+					},
+					success: function(res){
+						$('#position').html(res);
+					}
+				});
+		}	
+	});
 });
