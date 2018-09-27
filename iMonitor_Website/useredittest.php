a3a6a038ce69e4569afc2b261c7c2a07065ab5c0
orlancastelo <1040363@dt.com>
2018-09-27 08:21:39 +0800

    test

--- iMonitor_Website/useredittest.php
+++ iMonitor_Website/useredittest.php
@@ -42,8 +42,8 @@ $ID=$_GET['id'];
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="refresh" content="300">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	
     <!-- Our Custom CSS -->
     <link rel="stylesheet" href="style2.css">
@@ -232,7 +232,7 @@ $ID=$_GET['id'];
 						<td><?php echo $row['position']; ?></td>
 						<td><?php echo $row['role']; ?></td>
 						<td><?php echo $row['status']; ?></td>
-						<td><a href="#edit_<?php echo $row['id']; ?>" data-toggle="modal"><button class="btn btn-primary">Edit Record</button></a></td>
+						<td><a href="#edit_<?php echo $row['id']; ?>" data-toggle="modal"><button class="btn btn-info btn-lg">Edit Record</button></a></td>
 						<td><?php include('test_modal.php'); ?></td>
 					</tr>
 					<?php } ?>
