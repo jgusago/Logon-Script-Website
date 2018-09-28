8ec909bddcec6a52b61011574c9e1ae4c6113af4
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-09-28 08:25:55 +0800

    users dropdown notif

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -456,34 +456,51 @@ $ID=$_GET['id'];
 	</div>
 </form>-->
 <!-- End of Edit User Modal -->
+
+<script>
+
+$(document).ready(function(){
+	$(".dropdown").hover(            
+		function() {
+			$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+			$(this).toggleClass('open');        
+		},
+		function() {
+			$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+			$(this).toggleClass('open');       
+		}
+	);
+});
+
+		$(document).ready(function () {
+				$('#sidebarCollapse').on('click', function () {
+					$('#sidebar').toggleClass('active');
+				});
+			});
+
+
+
+	function isNumber(input) {
+	var regex =/[^0-9]/gi;
+	input.value = input.value.replace(regex,"");
+		
+	}
+
+
+	function lettersOnly(input) {
+	var regex = /[^a-z]/gi;
+	input.value = input.value.replace(regex,"");   
+}  
+
+}
+
+</script>   
 	
 </body>
 </html>
 
 <script>
-
-	$(document).ready(function()
-	{
-		$(".dropdown").hover(function() 
-		{
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
-            $(this).toggleClass('open');        
-        },
-		function() 
-		{
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-            $(this).toggleClass('open');       
-        });
-    });
-
-	(document).ready(function () 
-	{
-		$('#sidebarCollapse').on('click', function () 
-		{
-            $('#sidebar').toggleClass('active');
-        });
-	});
-					
+				
         function isNumber(input) {
           var regex =/[^0-9]/gi;
           input.value = input.value.replace(regex,"");
