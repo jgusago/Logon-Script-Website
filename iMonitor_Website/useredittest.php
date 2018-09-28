9b59452b12c2bbfed11e08a16e14e88ba8be7c84
orlancastelo <1040363@dt.com>
2018-09-28 08:15:06 +0800

    test

--- iMonitor_Website/useredittest.php
+++ iMonitor_Website/useredittest.php
@@ -39,7 +39,7 @@ $ID=$_GET['id'];
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="refresh" content="300">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
