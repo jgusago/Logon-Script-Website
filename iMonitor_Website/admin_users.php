ffa61bdf262ee9d6074ae614b8f86c05a3d8e3ed
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-10-01 07:13:27 +0800

    try

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -247,7 +247,7 @@ $ID=$_GET['id'];
     <div class="modal fade" id="addUser" tabindex="-1" role="dialog" aria-labelledby="myModallabel" arial-hidden="true" style="margin-top:150px;">
 		<div class="modal-dialog modal-md" role="document">
 			<div class="modal-content" style="width:550px;">
-				<div class="modal-header" style="background-color:#16811430; padding-bottom: 70px;"><b>User Registration</b>
+				<div class="modal-header" style="background-color:#16811430; padding-bottom: 10px;"><b>User Registration</b>
 					<button type="button" class="close" class="btn btn-default" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				</div>
 				<div class="body">
