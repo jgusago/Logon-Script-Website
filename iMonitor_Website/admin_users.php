c82fed86fc568b6bbbaf827cbe4149aa7186721f
orlancastelo <1040363@dt.com>
2018-10-03 10:04:30 +0800

    save

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -298,7 +298,7 @@ $ID=$_GET['id'];
 						<div class="form-group">
 							<label class="col-sm-12 control-label" for="department">Department</label>
 							<div class="col-sm-12">
-								<select class="form-control" id="department" name="department" required onchange="populatesecondtextbox();">
+								<select class="form-control" id="department" name="department" required>
 									<option value="">--Select Department--</option>
 										<?php     
               								$sql = "select DISTINCT branch_name from tbl_department ORDER BY branch_name ASC";
@@ -317,8 +317,7 @@ $ID=$_GET['id'];
 						<div class="form-group">
 							<label class="col-sm-12 control-label" for="position">Position</label>
 							<div class="col-sm-12">
-								<select class="form-control" id="position" name="position" required>
-								<option value="">--Select Position--</option>
+								<select class="form-control" id="position" name="position" required onchange="populatesecondtextbox();">
 								</select>
 							</div>
 						</div>
