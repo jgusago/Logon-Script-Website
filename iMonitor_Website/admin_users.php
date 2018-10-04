315b83502e0a8bdeaa5497456971d51721daf1a5
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-04 09:57:25 +0800

    AND > OR

--- iMonitor_Website/admin_users.php
+++ iMonitor_Website/admin_users.php
@@ -103,7 +103,7 @@ $ID=$_GET['id'];
 						<?php 
 							$d=strtotime("Now");		
 							$dateNow = date("Y-m-d h:i:sa", $d);
-                            $query = $db->prepare("SELECT user,hostname,iMonitor_Status,scan_time FROM tbl_log WHERE (iMonitor_Status = 'End Task' AND scan_time = '$dateNow' ) AND user != 'Administrator' LIMIT 5 ");
+                            $query = $db->prepare("SELECT user,hostname,iMonitor_Status,scan_time FROM tbl_log WHERE (iMonitor_Status = 'End Task' OR scan_time = '$dateNow' ) AND user != 'Administrator' LIMIT 5 ");
                             $query->execute();
                             $query->setFetchMode(PDO::FETCH_ASSOC);
                             while ($row = $query->fetch()) {
