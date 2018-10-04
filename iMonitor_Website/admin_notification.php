d826009c7518837a4b3a182a7db0afe4b265f5fe
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-04 13:28:15 +0800

    add

--- iMonitor_Website/admin_notification.php
+++ iMonitor_Website/admin_notification.php
@@ -206,7 +206,7 @@ exit();
                                     <tbody>
                                         <?php
                                             $d=strtotime("Now");		
-                                            $dateNow = date("d-Y", $d);
+                                            $dateNow = date("d-YY", $d);
                                             $query = $db->prepare("SELECT user,hostname,iMonitor_Status,connection_status,branch,scan_time FROM tbl_log WHERE iMonitor_Status = 'End Task' AND user != 'Administrator' ");
                                             $query->execute();
                                             $query->setFetchMode(PDO::FETCH_ASSOC);
@@ -224,12 +224,12 @@ exit();
                                             $query2->setFetchMode(PDO::FETCH_ASSOC);
                                             
                                             while ($row2 = $query2->fetch()) {
-                                                if(date("d-Y", strtotime($row2['scan_time'])) != $dateNow){
+                                                if(date("d-YY", strtotime($row2['scan_time'])) != $dateNow){
                                                     echo '
                                                     <tr>
                                                         <td>LogWindowsApp : Data Not Updated = '.$row2['scan_time'].'</td>
                                                         <td>Hostname: '.$row2['hostname'].' <br> User: '.$row2['user'].'<br/> Building : '.$row2['branch'].'</td>
-                                                        <td>'.date("d-Y", strtotime($row2['scan_time'])).'</td>
+                                                        <td>'.date("dd-YY", strtotime($row2['scan_time'])).'</td>
                                                     </tr>
                                                     ';
                                                 } 
