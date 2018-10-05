0a4ca045c7aeaf398c9df1f4f52fa02da36065d5
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-10-05 16:26:01 +0800

    html cache

--- iMonitor_Website/admin_viewing.php
+++ iMonitor_Website/admin_viewing.php
@@ -41,6 +41,9 @@ $ID=$_GET['id'];
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="refresh" content="300">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
