09daf951c7f7ca67bbd60252d5856a17d8c23446
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-10-10 07:20:32 +0800

    Update iMonitor_Website/index.php

--- iMonitor_Website/index.php
+++ iMonitor_Website/index.php
@@ -21,7 +21,7 @@ if(isset($_SESSION["userid"])) {
 <body style="height: -webkit-fill-available;">
     <div class="wrapper">
         <div class="col-lg-12">
-            <div>
+            <div style="height:175px;">
 
             </div>
             <br>
