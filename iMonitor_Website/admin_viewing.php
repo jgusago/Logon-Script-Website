3bd07d67e81438dd4c562aaafe05a542cb5457ed
orlancastelo <1040363@dt.com>
2018-10-09 15:12:08 +0800

    test

--- iMonitor_Website/admin_viewing.php
+++ iMonitor_Website/admin_viewing.php
@@ -240,7 +240,7 @@ $ID=$_GET['id'];
 
                                         <script>
                                             function handleSelect() {
-                                                if (this.value == "") {
+                                                if (this.value == null) {
                                                     document.getElementById('sub_dept_viewing').disabled = false;
                                                 } else {
                                                     document.getElementById('sub_dept_viewing').disabled = true;
@@ -250,7 +250,7 @@ $ID=$_GET['id'];
 
                                          <script>
                                             function handleSelect2() {
-                                                if (this.value == "") {
+                                                if (this.value == null) {
                                                     document.getElementById('sub_dept_viewing').disabled = true;
                                                 } else {
                                                     document.getElementById('sub_dept_viewing').disabled = false;
