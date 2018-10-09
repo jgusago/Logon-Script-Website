55fa40acc95cd8ed15ac3d07a3f2e1107bedef28
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-10-10 06:52:45 +0800

    try

--- iMonitor_Website/index.php
+++ iMonitor_Website/index.php
@@ -19,25 +19,25 @@ if(isset($_SESSION["userid"])) {
     <link href="../design/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <div class="header">
-        <?php
-            if(isset($_GET['msg']))                      
-            {                    
-                echo "<p align='center' class='error'>Wrong username or password.</p>";
-            }
-        ?>
-        <?php
-            if(isset($_GET['msg2']))                      
-            {                    
-                 echo "<p align='center' class='error'>Your account is Inactive.</p>";
-            }
-        ?>
-        <?php
-                //login();
-        ?>
-        <form action="../php/connection/login_submit.php" method="POST">
-            <div class="row col-sm-12 col-md-10  col-md-offset-1">
-                <div class="col-sm-12 col-md-10  col-md-offset-1">
+    <div class="wrapper">
+        <div class="col-lg-12">
+            <div class="panel">
+                <?php
+                    if(isset($_GET['msg']))                      
+                    {                    
+                        echo "<p align='center' class='error'>Wrong username or password.</p>";
+                    }
+                ?>
+                <?php
+                    if(isset($_GET['msg2']))                      
+                    {                    
+                        echo "<p align='center' class='error'>Your account is Inactive.</p>";
+                    }
+                ?>
+                <?php
+                        //login();
+                ?>
+                 <form action="../php/connection/login_submit.php" method="POST">
                     <div class="form-group">
                         <div class="input-group">
                             <span class="input-group-addon">
@@ -51,20 +51,20 @@ if(isset($_SESSION["userid"])) {
                             <span class="input-group-addon">
                                 <i class="glyphicon glyphicon-lock"></i>
                             </span>
-                            <!--<input class="form-control" id="password" placeholder="Password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" type="password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>-->
                             <input class="form-control" id="password" placeholder="Password" name="password" type="password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
                         </div>
                     </div>
-                    <div>
-                        <button type="submit" name="submit" class="btn btn-primary">Login</button></a>
-                        <!--<input type="submit" name="submit" class="btn btn-primary" />-->
+                    <div class="form-group">
+                        <div class="input-group">
+                            <button type="submit" name="submit" class="btn btn-primary">Login</button></a>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
-         </form>
-    </div>
-    <div>
-        <h1 style="padding-top:110px; font-family: Verdana, tahoma;color: white; text-align:center; font-size: 60px; margin-top: 790px;">SKYLUSTER TECHNOLOGY INC.</h1>
+        </div>
+        <div>
+            <h1 style="padding-top:110px; font-family: Verdana, tahoma;color: white; text-align:center; font-size: 60px; margin-top: 790px;">SKYLUSTER TECHNOLOGY INC.</h1>
+        </div>
     </div>
 
 </body>
