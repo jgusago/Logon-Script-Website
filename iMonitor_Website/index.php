c34f547c0eba08b0735bbf4672d88ccc063bff5c
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-10-11 07:59:23 +0800

    Update iMonitor_Website/index.php

--- iMonitor_Website/index.php
+++ iMonitor_Website/index.php
@@ -39,19 +39,19 @@ if(isset($_SESSION["userid"])) {
                             //login();
                     ?>
                     <form action="../php/connection/login_submit.php" method="POST">
-                        <div class="form-group input-group" style="margin-left: -80px; padding: 30px 20px;">
+                        <div class="form-group input-group" style="margin-left: -80px; padding: 30px 30px;">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                             <input class="form-control" id="userid" placeholder="User ID" name="userid" type="text" autofocus="autofocus" style="width: 150%;"required>
                         </div>
-                        <div class="form-group input-group" style="margin-left: -80px; padding: 30px 20px;">
+                        <div class="form-group input-group" style="margin-left: -80px; padding: 0px 30px;">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
                             <input class="form-control" id="password" placeholder="Password" name="password" type="password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" style="width: 150%;" required>
                         </div>
                         <div class="form-group">
-                            <button type="submit" name="submit" class="btn btn-primary btn-block" style="width:70%; margin-bottom:20px; margin-left:110px;">Login</button></a>
+                            <button type="submit" name="submit" class="btn btn-primary btn-block" style="width:70%; margin-left:70px;">Login</button></a>
                         </div>
                         <div class="form-group text-center">
-                            <a href="#">Forgot Password</a>
+                            <a href="#" style="color:white;">Forgot Password</a>
                         </div>
                     </form>
                 </div>
