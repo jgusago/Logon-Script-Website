2910c5464b23b3d9bf6cbf34933c69351f3597d3
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-10-11 07:53:37 +0800

    Update iMonitor_Website/index.php

--- iMonitor_Website/index.php
+++ iMonitor_Website/index.php
@@ -41,11 +41,11 @@ if(isset($_SESSION["userid"])) {
                     <form action="../php/connection/login_submit.php" method="POST">
                         <div class="form-group input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input class="form-control" id="userid" placeholder="User ID" name="userid" type="text" autofocus="autofocus" required>
+                            <input class="form-control" id="userid" placeholder="User ID" name="userid" type="text" autofocus="autofocus" style="width: 150%;"required>
                         </div>
                         <div class="form-group input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                            <input class="form-control" id="password" placeholder="Password" name="password" type="password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+                            <input class="form-control" id="password" placeholder="Password" name="password" type="password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" style="width: 150%;" required>
                         </div>
                         <div class="form-group">
                             <button type="submit" name="submit" class="btn btn-primary btn-block" style="width:70%; margin-bottom:20px; margin-left:110px;">Login</button></a>
