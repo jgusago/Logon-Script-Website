a4e864301ff64489e61728683eac4453f59b1cec
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-10-11 08:20:43 +0800

    Update iMonitor_Website/index.php

--- iMonitor_Website/index.php
+++ iMonitor_Website/index.php
@@ -22,40 +22,38 @@ if(isset($_SESSION["userid"])) {
     <div class="container">
         <div class="row">
             <div class="Absolute-Center is-Responsive">
-                <div class="panel" style="background-color:#e0dddd78;">
-                    <div class="col-sm-12 col-md-10 col-md-offset-1">
-                        <?php
-                            if(isset($_GET['msg']))                      
-                            {                    
-                                echo "<p align='center' class='error'>Wrong username or password.</p>";
-                            }
-                        ?>
-                        <?php
-                            if(isset($_GET['msg2']))                      
-                            {                    
-                                echo "<p align='center' class='error'>Your account is Inactive.</p>";
-                            }
-                        ?>
-                        <?php
-                                //login();
-                        ?>
-                        <form action="../php/connection/login_submit.php" method="POST">
-                            <div class="form-group input-group" style="margin-left: -90px; padding: 30px 30px; margin-bottom: 0px;">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <input class="form-control" id="userid" placeholder="User ID" name="userid" type="text" autofocus="autofocus" style="width: 150%;"required>
-                            </div>
-                            <div class="form-group input-group" style="margin-left: -90px; padding: 0px 30px;">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                <input class="form-control" id="password" placeholder="Password" name="password" type="password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" style="width: 150%;" required>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" name="submit" class="btn btn-primary btn-block" style="width:70%; margin-left:50px; position: inherit;">Login</button></a>
-                            </div>
-                            <div class="form-group text-center">
-                                <a href="#" style="color:white;">Forgot Password</a>
-                            </div>
-                        </form>
-                    </div>
+                <div class="col-sm-12 col-md-10 col-md-offset-1">
+                    <?php
+                        if(isset($_GET['msg']))                      
+                        {                    
+                            echo "<p align='center' class='error'>Wrong username or password.</p>";
+                        }
+                    ?>
+                    <?php
+                        if(isset($_GET['msg2']))                      
+                        {                    
+                            echo "<p align='center' class='error'>Your account is Inactive.</p>";
+                        }
+                    ?>
+                    <?php
+                            //login();
+                    ?>
+                    <form action="../php/connection/login_submit.php" method="POST">
+                        <div class="form-group input-group" style="margin-left: -90px; padding: 30px 30px; margin-bottom: 0px;">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input class="form-control" id="userid" placeholder="User ID" name="userid" type="text" autofocus="autofocus" style="width: 150%;"required>
+                        </div>
+                        <div class="form-group input-group" style="margin-left: -90px; padding: 0px 30px;">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                            <input class="form-control" id="password" placeholder="Password" name="password" type="password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" style="width: 150%;" required>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="submit" class="btn btn-primary btn-block" style="width:70%; margin-left:50px; position: inherit;">Login</button></a>
+                        </div>
+                        <div class="form-group text-center">
+                            <a href="#" style="color:white;">Forgot Password</a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
