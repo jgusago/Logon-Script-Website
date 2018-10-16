93eac98c8a0f1e3820b92196d4ff471512ee3598
Melvin <jgusago@gmail.com>
2018-10-16 16:33:45 +0800

    1

--- php/functions/sttngs/settings.branch.view.add.php
+++ php/functions/sttngs/settings.branch.view.add.php
@@ -2,14 +2,14 @@
 
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
-$query = "SELECT * FROM logonscript.tbl_tree WHERE treeparent like 'root'";
+$query = "SELECT * FROM logonscript.tbl_tree WHERE tree_parent like 'root'";
 $pdo = $db->prepare($query);
 $pdo->execute();
 $result = $pdo->fetchAll();
 $count = 0;
 foreach($result as $row){
-    $parent = $row['treename'];
-    $level = $row['treelevel'];
+    $parent = $row['tree_name'];
+    $level = $row['tree_level'];
 
     if ($count != 0){
         echo "|";
@@ -27,15 +27,15 @@ function child($parent,$child,$text){
 
     if($child < 3){
         require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
-        $query = "SELECT * FROM logonscript.tbl_tree WHERE treeparent LIKE :parent";
+        $query = "SELECT * FROM logonscript.tbl_tree WHERE tree_parent LIKE :parent";
         $pdo = $db->prepare($query);
         $pdo->bindParam(":parent",$parent);
         $pdo->execute();
         $result = $pdo->fetchAll();
 
         foreach($result as $row){
-            $parent2 = $row['treename'];
-            $level = $row['treelevel'];
+            $parent2 = $row['tree_name'];
+            $level = $row['tree_level'];
             $text = $text."->".$parent2;
             echo "|".$text;
 
