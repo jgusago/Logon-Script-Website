1357b806fc4ad1d33ed881f383c9fd111e4ef8c7
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-10-19 13:34:40 +0800

    try

--- js/controller/main.js
+++ js/controller/main.js
@@ -1067,8 +1067,8 @@ function dashboard_click_event(div)
     }
     thisdiv.removeAttribute("hidden","true");
     document.getElementById("panelid").remove();
-    document.getElementById("dtitle").innerHTML = "Reports";
 }
+
 
 // Mini Window Show Computer Details
 function miniwindow_computer_details(host){
