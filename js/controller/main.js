506adc339959b01df78cf6d4780d92df5eb1de1a
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-10-24 11:50:32 +0800

    Update js/controller/main.js

--- js/controller/main.js
+++ js/controller/main.js
@@ -522,14 +522,14 @@ function load_useraccount_table(parent, parentdiv)
         var th7 = document.createElement("th")
         th7.innerHTML = "Option";
 
-        var div3 = document.createElement("div");
-        div3.classList.add("panel-footer");
-        var btnadd = document.createElement("button");
-        btnadd.classList.add("btn");
-        btnadd.classList.add("btn-primary");
-        btnadd.createTextNode("Add user");
+        // var div3 = document.createElement("div");
+        // div3.classList.add("panel-footer");
+        // var btnadd = document.createElement("button");
+        // btnadd.classList.add("btn");
+        // btnadd.classList.add("btn-primary");
+        // btnadd.createTextNode("Add user");
 
-        div3.appendChild(btnadd);
+        // div3.appendChild(btnadd);
 
         tr.appendChild(th);
         tr.appendChild(th1);
@@ -548,7 +548,7 @@ function load_useraccount_table(parent, parentdiv)
         div.appendChild(div1);
         ctnview.appendChild(div);
         div.appendChild(div2);
-        div.appendChild(div3);
+        // div.appendChild(div3);
 
         div.style.background = "rgba(213, 213, 213, 0.3)";
         div.style.width = "-webkit-fill-available";
