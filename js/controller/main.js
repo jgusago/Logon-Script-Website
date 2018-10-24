fd4fdafa600e9e4a9ad3f5cb5dba6816147015bd
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-10-24 13:20:26 +0800

    Update js/controller/main.js

--- js/controller/main.js
+++ js/controller/main.js
@@ -543,8 +543,8 @@ function load_useraccount_table(parent, parentdiv)
         var btnedit = document.createElement("button");
         btnedit.classList.add("btn");
         btnedit.classList.add("btn-primary");
-        var btntxt = document.createTextNode("Edit User Record");
-        btntxt.setAttribute("id", "editUser");
+        // var btntxt = document.createTextNode("Edit User Record");
+        // btntxt.setAttribute("id", "editUser");
 
         var div3 = document.createElement("div");
         div3.classList.add("panel-footer");
@@ -558,7 +558,7 @@ function load_useraccount_table(parent, parentdiv)
         btnadd.appendChild(txtnode);
 
         td7.appendChild(btnedit);
-        btnedit.appendChild(btntxt);
+        // btnedit.appendChild(btntxt);
 
         tr.appendChild(th);
         tr.appendChild(th1);
