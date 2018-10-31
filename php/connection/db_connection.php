0f5d29e1eb3c499566d21670eea4f9e6e934b395
Jhon Mickelle Agumboy <32501053+jmickelle@users.noreply.github.com>
2018-10-30 20:01:11 -0700

    Update php/connection/db_connection.php

--- php/connection/db_connection.php
+++ php/connection/db_connection.php
@@ -1,5 +1,6 @@
 <?php
-    $db = new PDO('mysql:host=172.16.39.241;dbname=logonscript;charset=utf8mb4', 'administrator', 'Agsmc999');
+    //$db = new PDO('mysql:host=172.16.39.241;dbname=logonscript;charset=utf8mb4', 'administrator', 'Agsmc999');
+    $db = new PDO('mysql:host=172.16.31.39;dbname=logonscript;charset=utf8mb4', 'administrator', 'Agsmc999');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
