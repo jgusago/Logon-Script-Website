82bcdb596a44907a26696bc4ce67a38414186a44
John Melvin Gusago <42765192+jgusago@users.noreply.github.com>
2018-11-09 14:32:22 +0800

    Sorry :-1:

--- php/connection/db_connection.php
+++ php/connection/db_connection.php
@@ -1,8 +1,7 @@
 <?php
-    $db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=logonscript', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
+$db = new PDO('mysql:host=172.16.39.241;dbname=logonscript;charset=utf8mb4', 'administrator', 'Agsmc999');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     /*
     *$con = mysqli_connect('172.16.39.241','administrator','Agsmc999','logonscript');
     if(!$con){
