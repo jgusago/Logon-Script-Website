f087d357cd29d4ae2b6a52ae1b95d94167465614
Orlan Castelo <casteloorlan@gmail.com>
2018-11-14 17:41:37 +0800

    save

--- php/functions/reports/user.computer.list.php
+++ php/functions/reports/user.computer.list.php
@@ -1,11 +1,20 @@
 <?php 
-session_start();
-$department = $_SESSION["department"];
-require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
+    session_start();
+    $department = $_SESSION["department"];
+    require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
-		echo "Hostname|User|IP|Status|Remarks|Agent Version|Branch";
+    $sql2 = "SELECT * FROM logonscript.tbl_tree where tree_name like '$department'";
+      
+    foreach ($db->query($sql2) as $row) {
+        $tree_filter = $row['tree_filter'];
+    }    
 
-		$sql = "SELECT * from tbl_computer_details WHERE hostname like '%$tree_filter%'";
+		echo "Hostname|User|IP|Status|Remarks|Agent Version";
+
+		$sql = "SELECT logonscript.tbl_computer_details.hostname As hostname, logonscript.tbl_log.user AS user, logonscript.tbl_computer_details.ip AS IP, logonscript.tbl_computer_details.status AS status, logonscript.tbl_computer_details.remarks, logonscript.tbl_computer_details.agent_version as agent_version
+        FROM logonscript.tbl_computer_details 
+        INNER JOIN logonscript.tbl_log ON logonscript.tbl_log.hostname = tbl_computer_details.hostname
+        WHERE tbl_computer_details.hostname like '%$tree_filter%' group by tbl_computer_details.hostname";
 		foreach ($db->query($sql) as $row) {
 	
 			$hostname = $row['hostname'] ?: 'null';
@@ -14,9 +23,8 @@ require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
             $Status = $row['status'] ?: 'null';
             $remarks = $row['remarks'] ?: 'null';
             $agent_version = $row['agent_version'] ?: 'null';
-            $branch = $row['branch'] ?: 'null';
        
-			echo "#$hostname|$useer|$ip|$Status|$remarks|$agent_version|$branch";
+            echo "#$hostname|$useer|$ip|$Status|$remarks|$agent_version";
 
 		}
 	?>
