7d96b53afb961bba535d5b63eab7b155bdc7f03e
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-14 16:16:51 +0800

    try

--- js/controller/main.js
+++ js/controller/main.js
@@ -134,6 +134,60 @@ function DSHBRDContent(parent, linkid)
     }, 500)
 }
 
+function ACCTedit()
+{
+  
+  var divmodal = document.createElement("div");
+  divmodal.classList.add("modal");
+  divmodal.classList.add("fade");
+  divmodal.setAttribute("id", "EditUser");
+  divmodal.setAttribute("tabindex", "-1");
+  divmodal.setAttribute("role", "dialog");
+  divmodal.setAttribute("aria-labelledby", "myModalLabel");
+  divmodal.setAttribute("arial-hidden", "true");
+
+  var divmodaldg = documnet.createElement("div");
+  divmodaldg.classList.add("modal-dialog");
+  divmodaldg.classList.add("modal-md");
+  divmodaldg.setAttribute("role", "document");
+
+  divmodal.appendChild(divmodaldg);
+ 
+  var divmodalcnt = document.createElement("div");
+  divmodalcnt.classList.add("modal-content");
+
+  divmodaldg.appendChild(divmodalcnt);
+
+  var divmodalheader = document.createElement("div");
+  divmodalheader.classList.add("modal-header");
+  divmodalheader.classList.add("text");
+  divmodalheader.classList.add("center");
+
+  divmodalcnt.appendChild(divmodalheader);
+
+  var h4 = document.createElement("h4");
+  h4.classList.add("modal-title");
+  h4.setAttribute("id", "myModalLabel");
+  h4.innerHTML = "Edit User Information";
+  var btnclose = document.createElement("button");
+  btnclose.setAttribute("type", "button");
+  btnclose.setAttribute("data-dismiss", "modal");
+  btnclose.setAttribute("aria-label", "Clsoe");
+  btnclose.classList.add("btn");
+  btnclose.classList.add("btn-default");
+
+}
+
+
+
+
+
+
+
+
+
+
+
 //computerlist Update OnClick
 
 function COMPLISTupdate(hostname, user, remarks, tabledata, grandparent, linkid)
