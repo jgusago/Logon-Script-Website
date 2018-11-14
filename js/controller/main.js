f6e4f3459b6c169cd473423a425d39e3b2c0ef21
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-14 11:05:31 +0800

    try

--- js/controller/main.js
+++ js/controller/main.js
@@ -98,7 +98,7 @@ function DSHBRDContent(parent, linkid)
       case "DSHBRDAccountsAccMgnt":
         path = "php/functions/accounts/accounts.view.php";
         DSHBRDContentTbls(parent, path, table.head, table.foot, table.body, tableid, linkid);
-        createnewElement([], card.head, "button", ["btn","btn-default"],["data-toggle:modal", "onclick:AddUser()", "data-target:#AddUser", "href:#AddUser"],"Add User");
+        createnewElement([], card.head, "button", ["btn","btn-default"],["data-toggle:modal", "onclick:AddUser()", "data-target:#AddUser", "href:#AddUser", "id:btnAddUser"],"Add User");
         // <a href="#addUser" data-toggle="modal"><i class="glyphicon glyphicon-plus"></i><u>Add User</u></a>
       break;
       case "DSHBRDProfile":
@@ -132,6 +132,32 @@ function DSHBRDContent(parent, linkid)
       pdf[0].classList.add("btn-danger");
 
     }, 500)
+}
+
+function AddUser()
+{
+  var modal = document.getElementById("AddUser");
+  var btn = documnt.getElementById("btnAddUser");
+
+  var span = document.getElementsByClassName("close")[0];
+
+  btn.onClick = function()
+  {
+    modal.style.display ="none";
+  }
+
+  span.onClick = function()
+  {
+    modal.style.display ="none";
+  }
+
+  window.onclick = function(event)
+  {
+    if(event.target == "modal")
+    {
+      modal.style.display = "none";
+    }
+  }
 }
 
 //computerlist Update OnClick
