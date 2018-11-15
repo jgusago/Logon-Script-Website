50ea0310236939a0b598280e148e95aa718fdc95
Melvin <jgusago@gmail.com>
2018-11-15 22:02:15 +0800

    save

--- js/controller/admin.js
+++ js/controller/admin.js
@@ -200,7 +200,7 @@ function ACCTedit(userid, name, department, position, status, role)
 
   createnewElement(divbody2, cb, "div", ["md-form", "mb-3"], [], "");
   createnewElement(label2, divbody2.newelement, "label", [],[],"Department");
-  createnewElement(select, divbody2.newelement, "input", ["form-control"], ["type:text", "name:department", "id:department", "required"], "");
+  createnewElement(select, divbody2.newelement, "input", ["form-control"], ["type:text", "name:department", "id:department", "required", "value:"+department], "");
   // createnewElement(option, select.newelement, "option", [],[],"" );
 
   createnewElement(divbody3, cb, "div", ["md-form", "mb-3"], [], "");
