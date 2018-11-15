73140ec40cda1f368c81657e526305f668d26777
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-15 20:14:10 +0800

    try

---
+++ php/functions/accounts/user_account_submit2.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
+
+$userid = $_POST['userid'];
+
+if(!isset($error)){ 
+        $stmt = $db->prepare("SELECT userid FROM tbl_user WHERE userid = :userid");
+        $stmt->execute(array($userid));
+        $row_count = $stmt->rowCount();
+        
+        if ($row_count > 0){
+            echo "<script>alert('User ID is already exist!'); window.location='../../../.superadmin.html'</script>";
+        }
+else
+    {
+        //$hashed_password = password_hash($_POST["password2"],PASSWORD_DEFAULT);
+        $password = md5(sha1($password));
+
+        $sql = "INSERT INTO tbl_user (userid, name, department, role, status, password)
+             
+        VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["role"]."', '".$_POST["status"]."', '$password')";
+        ($db->query($sql));
+        echo "<script>alert('User Account Successfully Saved!'); window.location='../../../.superadmin.html'</script>";
+    }
+}
+?>
