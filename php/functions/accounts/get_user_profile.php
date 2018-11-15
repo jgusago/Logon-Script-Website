6394085e3ecd8dc7333cb261e83d45ac29ec8383
orlancastelo <casteloorlan@gmail.com>
2018-11-15 20:13:37 +0800

    save

--- php/functions/accounts/get_user_profile.php
+++ php/functions/accounts/get_user_profile.php
@@ -4,15 +4,14 @@ session_start();
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 ?>
 <?php
-	$sql = "SELECT id, userid, name, department, status, role FROM tbl_user WHERE role<>'SUPER ADMIN'";
+	$sql = "SELECT userid, name, department, status, role FROM tbl_user WHERE role<>'SUPER ADMIN'";
 		foreach ($db->query($sql) as $row) {
 			//$stmt = $db->query("SELECT id, userid, name, department, position, status, role FROM tbl_user WHERE role<>'SUPER ADMIN'");
 			//$stmt->execute();
 			//for($i=0; $row = $stmt->fetch(); $i++){
-			$id=$row['id'];			
+			$id=$row['userid'];			
 ?>
 					
-	<?php echo $row['id']; ?>
 	<?php echo $row['userid']; ?>
 	<?php echo $row['name']; ?>
 	<?php echo $row['department']; ?>
