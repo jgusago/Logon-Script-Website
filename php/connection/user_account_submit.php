6cfaea8a419ac08b1be956ddd5d0ab65be33f3f1
Orlan Castelo <casteloorlan@gmail.com>
2018-11-15 11:51:19 +0800

    save

--- php/connection/user_account_submit.php
+++ php/connection/user_account_submit.php
@@ -12,18 +12,18 @@ if(!isset($error)){
         $row_count = $stmt->rowCount();
         
         if ($row_count > 0){
-            echo "<script>alert('User ID is already exist!'); window.location='../../iMonitor_Website/admin_users.php'</script>";
+            echo "<script>alert('User ID is already exist!'); window.location='admin.html'</script>";
         }
 else
     {
         //$hashed_password = password_hash($_POST["password2"],PASSWORD_DEFAULT);
         $password = md5(sha1($password));
 
-        $sql = "INSERT INTO tbl_user (userid, name, department, position, role, status, password)
+        $sql = "INSERT INTO tbl_user (userid, name, department, role, status, password)
              
-        VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["position"]."', '".$_POST["role"]."', '".$_POST["status"]."', '$password')";
+        VALUES ('".$_POST["userid"]."', '".$_POST["name"]."', '".$_POST["department"]."', '".$_POST["role"]."', '".$_POST["status"]."', '$password')";
         ($db->query($sql));
-        echo "<script>alert('User Account Save Successfully!'); window.location='../../admin.html'</script>";
+        echo "<script>alert('User Account Save Successfully!'); window.location='admin.html'</script>";
     }
 }
 ?>
