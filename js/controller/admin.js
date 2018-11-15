8137c144be2422cd4a463a5b9402dd5ab850c397
Orlan Castelo <casteloorlan@gmail.com>
2018-11-15 10:47:22 +0800

    save

--- js/controller/admin.js
+++ js/controller/admin.js
@@ -464,6 +464,14 @@ function SESSIONConfirm(){
   });
 }
 
+/*Number Only*/
+function isNumberKey(evt)
+{
+  var charCode = (evt.which) ? evt.which : evt.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57))
+    return false;
+    return true;
+}
 //
 
 /* Background */
