f5197b242a3d989b7db0cfaa22b8db193d7f0aeb
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-15 14:31:07 +0800

    try

--- js/controller/admin.js
+++ js/controller/admin.js
@@ -134,6 +134,63 @@ function DSHBRDContent(parent, linkid)
     }, 500)
 }
 
+
+function ACCTedit()
+{
+  OVERLAYenable();
+
+  var ch = document.getElementById("mnch");
+  var cb = document.getElementById("mncb");
+  var cf = document.getElementById("mncf");
+
+  var value = [], divvalue = [], leftdiv = [], subrdiv = [], rightsidevalue = [], span = [], divbody = [], label = [], inputuid = [], divbody1 = [], inputname = [], label1 = [], divbody2= [], label2 = [], select = [], option = []
+  divbody3 = [], label3 = [], select1 = [], option1= [], option2 = [], option3 = [], divbody4 = [], label4 = [], select2 =[], options1 = [], options2=[], options3 = [], divbody5=[], label5 = [], inputpwd = [], divfooter = [], button =[];
+
+  createnewElement(divvalue, ch, "div", ["row"], [], "");
+  createnewElement(leftdiv, divvalue.newelement, "div", ["col-sm-12","col-md-8"], [], "");
+  createnewElement(value, leftdiv.newelement, "h4", [], [],"Edit User Registration" );
+  // createnewElement(rightdiv, divvalue.newelement, "button", ["col-sm-12","col-md-6","d-flex","flex-row-reverse"], [], "");
+
+  createnewElement(subrdiv, divvalue.newelement, "div", ["d-flex","flex-row-reverse", "col-md-4"], [], "");
+  createnewElement(rightsidevalue, subrdiv.newelement, "button", ["close", "btn", "btn-default"], ["data-dismiss:modal","aria-label:Close", "type:button", "onclick:OVERLAYdisable()"], "");
+  createnewElement(span, rightsidevalue.newelement, "span", [], ["aria-hidden:true"], "");
+  span.newelement.innerHTML = "&times;";
+
+  createnewElement(divbody, cb, "div", ["md-form", "mb-3"], [], "");
+  createnewElement(label, divbody.newelement, "label", [],[],"User ID");
+  createnewElement(inputuid, divbody.newelement, "input", ["form-control"], ["type:text", "id:userid", "required"], "");
+
+  createnewElement(divbody1, cb, "div", ["md-form", "mb-3"], [], "");
+  createnewElement(label1, divbody1.newelement, "label", [],[],"Name");
+  createnewElement(inputname, divbody1.newelement, "input", ["form-control"], ["type:text", "id:username", "required"], "");
+
+  createnewElement(divbody2, cb, "div", ["md-form", "mb-3"], [], "");
+  createnewElement(label2, divbody2.newelement, "label", [],[],"Department");
+  createnewElement(select, divbody2.newelement, "select", ["form-control"], ["name:department", "id:department", "required"], "");
+  createnewElement(option, select.newelement, "option", [],[],"" );
+
+  createnewElement(divbody3, cb, "div", ["md-form", "mb-3"], [], "");
+  createnewElement(label3, divbody3.newelement, "label", [],[],"Role");
+  createnewElement(select1, divbody3.newelement, "select", ["form-control"], ["name:role", "id:role", "required"], "");
+  createnewElement(option1, select1.newelement, "option", [],["value:"],"" );
+  createnewElement(option2, select1.newelement, "option", [],["value:ADMINISTRATOR"],"Administrator" );
+  createnewElement(option3, select1.newelement, "option", [],["value:STAFF"],"Staff" );
+
+  createnewElement(divbody4, cb, "div", ["md-form", "mb-3"], [], "");
+  createnewElement(label4, divbody4.newelement, "label", [],[],"Role");
+  createnewElement(select2, divbody4.newelement, "select", ["form-control"], ["name:status", "id:status", "required"], "");
+  createnewElement(options1, select1.newelement, "option", [],["value:"],"" );
+  createnewElement(options2, select2.newelement, "option", [],["value:Active"],"Active" );
+  createnewElement(options3, select2.newelement, "option", [],["value:Inactive"],"Inactive" );
+
+  createnewElement(divbody5, cb, "div", ["md-form", "mb-3"], [], "");
+  createnewElement(label5, divbody5.newelement, "label", [],[],"Password");
+  createnewElement(inputpwd, divbody5.newelement, "input", ["form-control"], ["type:password", "id:password", "required"], "");
+
+  createnewElement(divfooter, cf, "div", [], [], "");
+  createnewElement(button, divfooter.newelement, "input", ["btn", "btn-success"], ["value:Update", "type:submit", "id:btnUpdate", "name: btnUpdate"], "");
+}
+
 //computerlist Update OnClick
 
 function COMPLISTupdate(hostname, user, remarks, tabledata, grandparent, linkid)
