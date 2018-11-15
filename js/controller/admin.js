eb3dc8bece0a6c43520ac41bec7758275c56721d
Melvin <jgusago@gmail.com>
2018-11-15 14:00:54 +0800

    update

--- js/controller/admin.js
+++ js/controller/admin.js
@@ -440,9 +440,36 @@ function getNotification(){
     });
 }
 
-function NOTIFnotconnected(){
-  content = document.getElementById("contentview");
-  $.post("");
+function NOTIFnotconnected(){    
+  var view = document.getElementById("contentview");
+  view.innerHTML = "";
+
+  tableid = idgenerator();
+
+  var card = [];
+  createCard(card, view, [], []);
+
+  var table = [];
+  var classes = ["table","table-bordered"];
+  var attributes = ["width:100%","cellspacing:0","id:"+tableid];
+  createTable(table, card.body, classes, attributes);
+  $.post("php/functions/notification/notification.notconnected.summary.php",function(data){
+
+    data = data.split("#");
+    datalength = data.length;
+
+    thfdata = data[0].split("|");
+    var tbheader = [], tbfooter = [];
+    createTableContent([], table.head, [], [], "th", thfdata);
+    createTableContent([], table.foot, [], [], "th", thfdata);
+
+    for (var i = 1; i < datalength;i++){
+        newdata = data[i].split("|");
+        createTableContent([], table.body, [],[], "td", newdata);
+
+        }
+
+  });
 }
 
 function SESSIONConfirm(){
--- php/functions/notification/notification.notconnected.summary.php
+++ php/functions/notification/notification.notconnected.summary.php
@@ -18,15 +18,36 @@ else{
     else{
         $filter = "123123123123";
     }
-    echo "$filter<br>";
 
     $sql = "SELECT * FROM logonscript.tbl_log WHERE hostname LIKE '%$filter%' AND connection_status not like 'ESTABLISHED' or iMonitor_Status not like 'running'";
 }
-echo $sql."<br>";
-    foreach ($db->query($sql) as $row){
-        $hostname = $row['hostname'];
-        echo "$hostname<br>";
-    }
 
-    echo $_SESSION['role'];
+echo "Computer Name|User|Domain|IP Address|Services Status|Server Status|Branch|Scan Time";
+foreach ($db->query($sql) as $row){
+    $hostname = $row['hostname'] ?: 'null';
+    $user = $row['user'] ?: 'null';
+    $domain_name = $row['domain_name'] ?: 'null';
+    $ip_address = $row['ip_address'] ?: 'null';
+    $ip_date_modefied = $row['ip_date_modified'] ?: 'null';
+    $iMonitor_Status = $row['iMonitor_Status'] ?: 'Not Found';
+    $services = $row['services'] ?: 'All Running';
+    $sysSetting_File = $row['sysSetting_File'] ?: 'Not Found';
+    $serverIP = $row['serverIP'] ?: 'Not Found';
+    $connections_status = $row['connection_status'] ?: 'Not Connected';
+    $branch = $row['branch'] ?: 'Scan Failed';
+    $scan_time = $row['scan_time'] ?: 'null';
+    $date = $scan_time;
+
+    $date = explode(" ",$scan_time);
+
+    $date[0] = preg_replace("/[^a-zA-Z]/", "", $date[0]);
+
+    if ($newdate = new DateTime($date[0]." ".$date[1])){
+
+    $scan_time = date_format($newdate, "M-d-Y H:i");
+    }
+    echo "#$hostname|$user|$domain_name|$ip_address~$ip_date_modefied|iMonitor Status: $iMonitor_Status~Missing Services: $services~Config: $sysSetting_File|Server IP: $serverIP~Connection Status: $connections_status|$branch|$scan_time";
+
+}
+
 ?>
