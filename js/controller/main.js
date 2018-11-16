a425ffa3d9e871a8e5bb4e17b2e24e1bead4c225
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-16 12:47:01 +0800

    Update js/controller/main.js

--- js/controller/main.js
+++ js/controller/main.js
@@ -153,8 +153,9 @@ function ACCTedit()
   divbody2= [], label2 = [], select = [], option = []
   divbody3 = [], label3 = [], select1 = [], option1= [], option2 = [], option3 = [], 
   divbody4 = [], label4 = [], select2 =[], options1 = [], options2=[], options3 = [], 
-  divbody5=[], label5 = [], inputpwd = [], divfooter = [], button = [];
-  // divbody7 = [], checkbox = [], label7 =[], inputcb =[];
+  divbody5=[], label5 = [], inputpwd = [], 
+  // divfooter = [], button = [];
+   divbody7 = [], checkbox = [], label7 =[], inputcb =[];
 
   // Modal Header
   createnewElement(divvalue, ch, "div", ["row"], [], "");
@@ -200,8 +201,8 @@ function ACCTedit()
   createnewElement(inputpwd, divbody5.newelement, "input", ["form-control"], ["type:password", "id:password", "required"], "");
 
   createnewElement(divbody7, cb, "div",["md-form", "mb-3"], [], "");
-  createnewElement(checkbox, divbody5.newelement, "checkbox", [],[],"");
-  createnewElement(label7, divbody5.newelement, "label", [], [], "");
+  createnewElement(checkbox, divbody7.newelement, "checkbox", [], [],"");
+  createnewElement(label7, divbody7.newelement, "label", [], [], "");
   createnewElement(inputcb, label7.newelement, [],["type:checkbox", "onclick:resetPass()"],"Reset Password");
 
   // Modal Footer
