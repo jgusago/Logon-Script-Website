45a529086b3e8d0a3718d961c22be19d8686a675
Orlan Castelo <casteloorlan@gmail.com>
2018-11-20 14:54:40 +0800

    save

--- js/controller/admin.js
+++ js/controller/admin.js
@@ -185,7 +185,7 @@ function ACCTedit(userid, name, department, position, role, status)
   createnewElement(inputpwd, divbody5.newelement, "input", ["form-control"], ["type:password", "id:password", "required"], "");
 
   createnewElement(divfooter, cf, "div", [], [], "");
-  createnewElement(button, divfooter.newelement, "input", ["btn", "btn-success"], ["value:Update", "type:submit", "id:btnUpdate", "name: btnUpdate"], ["id:UserAccountupdate","onclick:UserAccountupdate(\""+userid+"\",\""+tabledata+"\",\""+grandparent+"\",\""+linkid+"\")"], "");
+  createnewElement(button, divfooter.newelement, "input", ["btn", "btn-success"], ["value:Update", "type:submit", "id:btnUpdate", "name: btnUpdate"], "");
 }
 
 //computerlist Update OnClick
