76ce1260bbc7528e8c92447c4c100d497d0864ce
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-20 15:21:54 +0800

    Update js/controller/admin.js

--- js/controller/admin.js
+++ js/controller/admin.js
@@ -171,8 +171,8 @@ function ACCTedit(userid, name, department, position, role, status)
   createnewElement(label3, divbody3.newelement, "label", [],[],"Role");
   createnewElement(select1, divbody3.newelement, "select", ["form-control"], ["name:role", "id:role", "required", "value:"+role], "");
   createnewElement(option1, select1.newelement, "option", [],["value:"+role,"hidden:true","selected:selected"], role);
-  createnewElement(option2, select1.newelement, "option", [],["value:ADMINISTRATOR"],"Administrator" );
-  createnewElement(option3, select1.newelement, "option", [],["value:STAFF"],"Staff" );
+  // createnewElement(option2, select1.newelement, "option", [],["value:ADMINISTRATOR"],"Administrator" );
+  // createnewElement(option3, select1.newelement, "option", [],["value:STAFF"],"Staff" );
 
   createnewElement(divbody4, cb, "div", ["md-form", "mb-3"], [], "");
   createnewElement(label4, divbody4.newelement, "label", [],[],"Status");
