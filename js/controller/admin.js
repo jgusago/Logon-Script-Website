b9430aacc33c9f475a795cc7d91dfaca9f265175
Orlan Castelo <casteloorlan@gmail.com>
2018-11-20 14:42:35 +0800

    save

--- js/controller/admin.js
+++ js/controller/admin.js
@@ -223,7 +223,7 @@ function ACCTedit(userid, name, department, position, role, status)
   createnewElement(inputpwd, divbody5.newelement, "input", ["form-control"], ["type:password", "id:password", "required"], "");
 
   createnewElement(divfooter, cf, "div", [], [], "");
-  createnewElement(button, divfooter.newelement, "input", ["btn", "btn-success"], ["value:Update", "type:submit", "id:btnUpdate", "name: btnUpdate"], "");
+  createnewElement(button, divfooter.newelement, "input", ["btn", "btn-success"], ["value:Update", "type:submit", "id:btnUpdate", "name: btnUpdate"], ["id:UserAccountupdate","onclick:UserAccountupdate(\""+userid+"\",\""+tabledata+"\",\""+grandparent+"\",\""+linkid+"\")"], "");
 }
 
 //computerlist Update OnClick
@@ -470,6 +470,26 @@ function CMPLISTdtlsupdate(hostname, update, grandparent, linkid){
   //view.innerHTML = data;
   });
   CMPLISTdtlstableupdate(grandparent,linkid);
+  //DSHBRDRecordsComplist
+  //CMPLISTdtlsupdate(linkid);
+  OVERLAYdisable();
+}
+
+/*User Account Update*/
+
+function UserAccountupdate(userid, update, grandparent, linkid){
+
+  //var e = document.getElementById("CMPLISTdtlsremarks");
+  //var i = e.selectedIndex;
+  //var remarks = e.options[i].text;
+
+  //var agentversion = document.getElementById("CMPLISTdtlsagentversion").value;
+
+  $.post("php/functions/reports/user.account.update.php",{name:name,department:department,position:position,role:role,status:Status,password:password,update:update},function(data){
+  //var view = document.getElementById("contentview");
+  //view.innerHTML = data;
+  });
+  UserAccountupdate(grandparent,linkid);
   //DSHBRDRecordsComplist
   //CMPLISTdtlsupdate(linkid);
   OVERLAYdisable();
