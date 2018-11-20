3787b3803eeabef060a61ac2e99d2b82bd742916
Orlan Castelo <casteloorlan@gmail.com>
2018-11-20 17:34:24 +0800

    save

--- php/functions/reports/user.account.update.php
+++ php/functions/reports/user.account.update.php
@@ -6,20 +6,8 @@ $position = $_POST['positon'];
 $role = $_POST['role'];
 $status = $_POST['Status'];
 $password = $_POST['password'];
-//$hostname = "AEITOM073137";
-//$remarks = "test";
-//$version = "123456";
+
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
-
-//echo "$remarks<br>$version<br>$hostname<br>$update<br>";
-
-
-//$sql = "SELECT * FROM logonscript.tbl_computer_details WHERE hostname like '$hostname'";
-//foreach ($db->query($sql) as $row){
-//$oldversion = $row['agent_version'];
-//$oldremarks = $row['remarks'];
-//}
-
 
 //Check if already have a record
 if ($update == "true"){
@@ -42,11 +30,6 @@ if ($update == "true"){
         $pdo->execute();
     }
 }
-//else{
-    //$query2 = "INSERT INTO tbl_computer_details (hostname, processor, hdd_Serial, mac_Address, mb_manufacturer, mb_product, scan_time, ip, status, remarks, agent_version) VALUES (?, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, ?, ?)";
-    //$pdo2 = $db->prepare($query2);
-    //$pdo2->execute([$hostname, $remarks, $version]);
-//}
 
 $pdo = null;
 $pdo2 = null;
