27d60c8c6bacf1f831a4818f4662f01b1d997a7b
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-21 15:00:54 +0800

    Update accounts.view.php

--- php/functions/accounts/accounts.view.php
+++ php/functions/accounts/accounts.view.php
@@ -9,11 +9,23 @@ session_start();
 	{
 
 		$sql = "SELECT userid, name, department, position, role, status FROM tbl_user WHERE role<>'SUPER ADMIN'";
+
+		foreach ($db->query($sql) as $row) 
+		{
+
+			$userid = $row['userid'] ?: 'null';
+			$name = $row['name'] ?: 'null';
+			$department = $row['department'] ?: 'null';
+			$position = $row['position'] ?: 'null';
+			$role = $row['role'] ?: 'null';
+			$status = $row['status'] ?: 'null';
+
+			echo "#$userid|$name|$department|$position|$role|$status|button`btn~btn-primary`onclick:ACCTedit(\"$userid\",\"$name\",\"$department\",\"$position\",\"$role\",\"$status\")`Edit`";
 	}
 	else
 		$sql1 = "SELECT userid, name, department, position, role, status FROM tbl_user";
 
-		foreach ($db->query($sql) as $row) 
+		foreach ($db->query($sql1) as $row) 
 		{
 
 			$userid = $row['userid'] ?: 'null';
