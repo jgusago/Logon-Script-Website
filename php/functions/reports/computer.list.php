7af2043bbc0311c6c28eb2c6ad65d14185821921
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-22 14:41:49 +0800

    Update computer.list.php

--- php/functions/reports/computer.list.php
+++ php/functions/reports/computer.list.php
@@ -27,7 +27,7 @@ else{
         }    
         $query = "SELECT * from logonscript.tbl_log WHERE hostname like '%$tree_filter%'";
 }
-echo "Computer Name|User|Employee ID|IP Address|Services Status|Server Status|Remarks|Agent Version|Scan Time|Confirm Date|Last Confirm DateAction";
+echo "Computer Name|User|Employee ID|IP Address|Services Status|Server Status|Remarks|Agent Version|Scan Time|Confirm Date|Last Confirm Date|Action";
 
 $pdo = $db->prepare($query);
 $pdo->bindParam(":parent",$parent);
