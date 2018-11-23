8f648aa096855f1b49be9229a3bf106a26eed5c9
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-23 13:52:24 +0800

    Update user.computer.list.php

--- php/functions/reports/user.computer.list.php
+++ php/functions/reports/user.computer.list.php
@@ -14,7 +14,7 @@
 		$sql = "SELECT logonscript.tbl_computer_details.hostname As hostname, logonscript.tbl_log.user AS user, logonscript.tbl_computer_details.ip AS ip, logonscript.tbl_computer_details.status AS status, logonscript.tbl_computer_details.remarks, logonscript.tbl_computer_details.agent_version as agent_version
         FROM logonscript.tbl_computer_details 
         INNER JOIN logonscript.tbl_log ON logonscript.tbl_log.hostname = tbl_computer_details.hostname
-        WHERE tbl_computer_details.branch like '%$tree_filter%' group by tbl_computer_details.hostname";
+        WHERE tbl_computer_details.branch like '%$tree_filter%' group by tbl_computer_details.branch";
 		foreach ($db->query($sql) as $row) {
 	
 			$hostname = $row['hostname'] ?: 'null';
