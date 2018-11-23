8e50a293db1cf1f73a737887bd595699be281d05
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-23 17:57:22 +0800

    Update computer.logs.history.php

--- php/functions/reports/computer.logs.history.php
+++ php/functions/reports/computer.logs.history.php
@@ -15,19 +15,11 @@ if($_SESSION['role'] != "STAFF")
         $services = $row['services'];
         $branch = $row['branch'];
         $time = $row['scan_time'];
-        $date = $time;
 
         $query3 = "SELECT hostname from logonscript.tbl_log WHERE ip_address LIKE '$ip'";
         foreach ($db->query($query3) as $row)
          {
             $hostname = $row['hostname'];
-
-            $date[0] = preg_replace("/[^a-zA-Z]/", "", $date[0]);
-    
-            if ($newdate = new DateTime($date[0]." ".$date[1]))
-            {
-                $time = date_format($newdate, "M-d-Y H:i");
-            }
         }
 
         echo "#$hostname|$id|$ip|$services|$branch|$time";
@@ -49,13 +41,6 @@ else
         foreach ($db->query($query4) as $row) 
         {
             $hostname = $row['hostname'];
-
-            $date[0] = preg_replace("/[^a-zA-Z]/", "", $date[0]);
-    
-            if ($newdate = new DateTime($date[0]." ".$date[1]))
-            {
-                $time = date_format($newdate, "M-d-Y H:i");
-            }
         }
 
         echo "#$hostname|$id|$ip|$services|$branch|$time";
