6f22fccacdc30f7c550b82124a46ceee45eff85d
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-23 10:34:28 +0800

    Update admin.js

--- js/controller/admin.js
+++ js/controller/admin.js
@@ -1,7 +1,7 @@
 /* -------------------------------------------------------------------------- Loads ---------------------------------------------------------------------------------- */
 function load(){
     SESSIONConfirm();
-
+  
     var branchview = document.getElementById("contentview");
     var loading = document.getElementById("processingbar");
 
@@ -73,16 +73,19 @@ function DSHBRDContent(parent, linkid)
     var attributes = ["width:100%","cellspacing:0","id:"+tableid];
     createTable(table, card.body, classes, attributes);
 
-    switch (linkdata)
+    switch (linkdata) 
     {
       case "DSHBRDRecordsComplist":
         path = "php/functions/reports/computer.list.php";
         DSHBRDContentTbls(parent, path, table.head, table.foot, table.body, tableid, linkid);
-
+        var reports = document.getElementById("Reports");
+        reports.classList.add("active");
       break;
       case "DSHBRDRecordsComplogs":
         path = "php/functions/reports/computer.logs.php";
         DSHBRDContentTbls(parent, path, table.head, table.foot, table.body, tableid, linkid);
+        var reports = document.getElementById("Reports");
+        reports.classList.add("active");
       break;
       case "DSHBRDRecordsHistory":
         path = "";
@@ -104,7 +107,7 @@ function DSHBRDContent(parent, linkid)
       default:
 
     }
-
+    
     setTimeout(function()
     {
       var copy = document.getElementsByClassName("buttons-copy");
@@ -133,7 +136,7 @@ function ACCTedit(userid, name, department, position, role, status, tabledata, g
   var cf = document.getElementById("mncf");
 
   var value = [], divvalue = [], leftdiv = [], subrdiv = [], rightsidevalue = [], span = [], divbody = [], label = [], inputuid = [], divbody1 = [], inputname = [], label1 = [], divbody2= [], label2 = [], select = [], option = [],
-  divbody3 = [], label3 = [], select1 = [], option1= [], option2 = [], option3 = [], divbody4 = [], label4 = [], select2 =[], options1 = [], options2=[], options3 = [], divbody5=[], label5 = [], inputpwd = [],
+  divbody3 = [], label3 = [], select1 = [], option1= [], option2 = [], option3 = [], divbody4 = [], label4 = [], select2 =[], options1 = [], options2=[], options3 = [], divbody5=[], label5 = [], inputpwd = [], 
   divfooter = [], button = [], divbody6 =[], label6 =[];
 
   //whole div
@@ -142,7 +145,7 @@ function ACCTedit(userid, name, department, position, role, status, tabledata, g
   createnewElement(leftdiv, divvalue.newelement, "div", ["col-sm-12","col-md-6"], [], "");
     //leftside contents
     createnewElement(value, leftdiv.newelement, "h4", [], [],"");
-
+   
 
   createnewElement(divvalue, ch, "div", ["row"], [], "");
   createnewElement(leftdiv, divvalue.newelement, "div", ["col-sm-12","col-md-8"], [], "");
@@ -190,7 +193,7 @@ function ACCTedit(userid, name, department, position, role, status, tabledata, g
   createnewElement(inputpwd, divbody6.newelement, "input", ["form-control"], ["type:password", "id:password", "required:true"], "");
 
   createnewElement(divfooter, cf, "div", [], [], "");
-  createnewElement(button, divfooter.newelement, "input", ["btn", "btn-success"], ["value:Update", "type:submit", "id:btnUpdate", "name: btnUpdate", "id:UserAccountupdate", "onclick:UserAccountupdate(\""+userid+"\",\""+tabledata+"\",\""+grandparent+"\",\""+linkid+"\")"], "");
+  createnewElement(button, divfooter.newelement, "input", ["btn", "btn-success"], ["value:Update", "type:submit", "name: btnUpdate", "id:UserAccountupdate", "onclick:UserAccountupdate(\""+userid+"\",\""+tabledata+"\",\""+grandparent+"\",\""+linkid+"\")"], "");
 }
 
 //computerlist Update OnClick
@@ -327,7 +330,7 @@ function DSHBRDContentBranchSettings()
   createTable(table, card.body, classes, attributes);
   $.post("php/functions/sttngs/settings.branch.view.php",function(data){
     data = data.split("||");
-    for(var arraccount = 0; arraccount < data.length; arraccount++){
+    for(var arraccount = 0; arraccount < data.length; arraccount++){ 
       var currentdata = data[arraccount].split(";");
 
       if (currentdata[2] == "tr"){
@@ -364,27 +367,11 @@ function addbranch(){
   createnewElement(headercontainer,ch, "div", ["container"], [], "");
 
   createnewElement(headerrow, headercontainer.newelement ,"div",["row"],[],"");
-  createnewElement(headerdiv1, headerrow.newelement,"div",["col-sm-10","col-md-10"],[],"Add Branch/Department");
-  headerdiv1.newelement.style.width = "600px";
+  createnewElement(headerdiv1, headerrow.newelement,"div",["col-sm-12","col-md-6"],[],"Add Branch/Deparetment");
   var rightsidevalue = [], span = [], headerdiv2 = [];
-  createnewElement(headerdiv2, headerrow.newelement, "div", ["col-sm-2","col-md-2"],[],"");
+  createnewElement(headerdiv2, ch, "div", ["col-sm-12","col-md-6"],[],"");
   createnewElement(rightsidevalue, headerdiv2.newelement, "a", ["nav-link"], ["aria-expanded:false","href:#", "onclick:OVERLAYdisable()"], "");
   createnewElement(span, rightsidevalue.newelement, "i", ["fa","fa-lg","fa-fw","fa-times"], [], "");
-
-  var form = [], formrow = [], label = [], col1 = [], col2 = [], col3 = [], col4 = [], select1 = [], select2 = [], select3 = [], select4 = [], option1 = [], option2 = [], option3 = [], option4 = [];
-  //createform
-  createnewElement(form, db, "form", [], [], "");
-  //create form-row
-  createnewElement(formrow, form.newelement, "div", ["form-row"],[],"");
-  //label
-  createnewElement(label, formrow.newelement, "lable",[],[],"Add Branch/Department/Sub-Department");
-  //first col
-  createElement(col1, formrow.newelement, "div", ["col-md-3","mb3"],[],[],"");
-    //Select
-    createnewElement(select1, col1.newelement, "select", [], [], "");
-    createnewElement(option1, select1.newelement, "option", [], ["selected:selected","hidden:true"],"Add Here");
-
-
 
 }
 /*End of Branch View*/
@@ -466,41 +453,21 @@ function CMPLISTdtlsupdate(hostname, update, grandparent, linkid){
 /*User Account Update*/
 
 function UserAccountupdate(userid, update, grandparent, linkid){
-  userid=document.getElementById("userid").value;
-  name=document.getElementById("name").value;
-  department=document.getElementById("department").value;
-  position=document.getElementById("position").value;
-  role=document.getElementById("role").value;
-  status=document.getElementById("status").value;
-  password=document.getElementById("password").value;
 
-  $.post("php/functions/reports/user.account.update.php",{name:name,department:department,position:position,role:role,status:status,password:password},function(data){
+  //var e = document.getElementById("CMPLISTdtlsremarks");
+  //var i = e.selectedIndex;
+  //var remarks = e.options[i].text;
 
+  //var agentversion = document.getElementById("CMPLISTdtlsagentversion").value;
+
+  $.post("php/functions/reports/user.account.update.php",{name:name,department:department,position:position,role:role,status:status,password:password,update:update},function(data){
+  //var view = document.getElementById("contentview");
+  //view.innerHTML = data;
   });
-  UserAccounttableupdate(grandparent,linkid);
-
+  UserAccountupdate(grandparent,linkid);
+  //DSHBRDRecordsComplist
+  //CMPLISTdtlsupdate(linkid);
   OVERLAYdisable();
-}
-
-/*User Account View*/
-
-function UserAccounttableupdate(parent, linkid){
-  var view = document.getElementById("contentview");
-  view.innerHTML = "";
-
-  tableid = idgenerator();
-
-  var card = [];
-  createCard(card, view, [], []);
-
-  var table = [];
-  var classes = ["table","table-bordered"];
-  var attributes = ["width:100%","cellspacing:0","id:"+tableid];
-  createTable(table, card.body, classes, attributes);
-  path = "php/functions/accounts/accounts.view.php";
-  setTimeout(function(){
-     DSHBRDContentTbls(parent, path, table.head, table.foot, table.body, tableid, linkid);
-   }, 100);
 }
 
 function CMPLISTdtlstableupdate(parent, linkid){
@@ -569,11 +536,11 @@ function getNotification(){
         newdiv.innerHTML = data[1];
         content.appendChild(newdiv);
       }
-
+    
     });
 }
 
-function NOTIFnotconnected(){
+function NOTIFnotconnected(){    
   var view = document.getElementById("contentview");
   view.innerHTML = "";
 
@@ -723,7 +690,7 @@ function SESSIONConfirm(){
     }
 
     var nametext = document.createTextNode(data[3]);
-    name.appendChild(nametext);
+    name.appendChild(nametext); 
   });
 }
 
@@ -737,7 +704,7 @@ function isNumberKey(evt)
 }
 
 /*Letters Only*/
-function LettersrOnly(e)
+function LettersrOnly(e) 
 		{
       var arr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
       var code;
