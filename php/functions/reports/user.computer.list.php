5a12e5154b30d1f347e1d79e2974a951fc98b969
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-23 13:51:07 +0800

    wadawd

--- php/functions/reports/user.computer.list.php
+++ php/functions/reports/user.computer.list.php
@@ -6,7 +6,7 @@
     $sql2 = "SELECT * FROM logonscript.tbl_tree where tree_name like '$department'";
       
     foreach ($db->query($sql2) as $row) {
-        $tree_filter = $row['tree_filter'];
+        $tree_filter = $row['tree_name'];
     }    
 
 		echo "Hostname|User|IP|Status|Remarks|Agent Version";
@@ -14,7 +14,7 @@
 		$sql = "SELECT logonscript.tbl_computer_details.hostname As hostname, logonscript.tbl_log.user AS user, logonscript.tbl_computer_details.ip AS ip, logonscript.tbl_computer_details.status AS status, logonscript.tbl_computer_details.remarks, logonscript.tbl_computer_details.agent_version as agent_version
         FROM logonscript.tbl_computer_details 
         INNER JOIN logonscript.tbl_log ON logonscript.tbl_log.hostname = tbl_computer_details.hostname
-        WHERE tbl_computer_details.hostname like '%$tree_filter%' group by tbl_computer_details.hostname";
+        WHERE tbl_computer_details.branch like '%$tree_filter%' group by tbl_computer_details.hostname";
 		foreach ($db->query($sql) as $row) {
 	
 			$hostname = $row['hostname'] ?: 'null';
