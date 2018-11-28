4aa212ac3f7c75caf3f9d48527fe3bbce0b4bf82
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-28 13:45:37 +0800

    Update admin.js

--- js/controller/admin.js
+++ js/controller/admin.js
@@ -209,13 +209,6 @@ function AgentUpdate(hostname)
   var divvalue = [], leftdiv = [], value = [], subrdiv =[], rightsidevalue =[], span =[];
 
   createnewElement(divvalue, ch, "div", ["row"], [], "");
-  //leftside div
-  createnewElement(leftdiv, divvalue.newelement, "div", ["col-sm-12","col-md-12"], [], "");
-    //leftside contents
-  createnewElement(value, leftdiv.newelement, "h4", [], [],"");
-
-
-  createnewElement(divvalue, ch, "div", ["row"], [], "");
   divvalue.newelement.style.width = "600px";
   createnewElement(leftdiv, divvalue.newelement, "div", ["col-sm-12","col-md-8"], [], "");
   createnewElement(value, leftdiv.newelement, "h4", [], [],"" );
