779897e88743a73282baf0c90b68ff80f1535bd9
John Melvin Gusago <42765192+jgusago@users.noreply.github.com>
2018-11-28 13:52:33 +0800

    Update settings.branch.view.path.selection.php

--- php/functions/sttngs/settings.branch.view.path.selection.php
+++ php/functions/sttngs/settings.branch.view.path.selection.php
@@ -1,9 +1,29 @@
 <?php
 
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
+$branch = "6789(MK)";
+//$branch = $_POST['branch'];
+echo $branch."<br>";
+$query = "SELECT * FROM logonscript.tbl_tree WHERE tree_parent LIKE '$branch'";
+foreach ($db->query($query) as $row) {
+  $path = "";
+  $tree_name = $row['tree_name'];
+  echo $path = $branch."/".$tree_name;
+  echo "<br>";
+  children($tree_name, $path);
+}
 
-$branch = $_POST['branch'];
+function children($branch, $path){
 
+  require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
+  $query = "SELECT * FROM logonscript.tbl_tree WHERE tree_parent LIKE '$branch'";
+  foreach ($db->query($query) as $row) {
+    $tree_name = $row['tree_name'];
+    echo $newpath = $path."/".$tree_name;
+    echo "<br>";
+    children($tree_name, $newpath);
+  }
+}
 
 ?>
