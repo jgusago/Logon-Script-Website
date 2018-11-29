25a3c084364c0a3471c75eac428f236b7e191094
Orlan Castelo <casteloorlan@gmail.com>
2018-11-29 16:27:30 +0800

    save

--- php/functions/accounts/user_account_submit.php
+++ php/functions/accounts/user_account_submit.php
@@ -32,6 +32,7 @@ else
         VALUES ('$AddUser', 'UserID:".$_POST["userid"].",Name:".$_POST["name"].",Department:".$_POST["department"].",Role:".$_POST["role"].",Status:".$_POST["status"]."', NOW(), '$userid2')";
         ($db->query($sql2));
 
+        echo "$encrypt_password";
         echo "<script>alert('User Account Save Successfully!'); window.location='../../../.admin.html'</script>";
 
         //if(!$sql)
