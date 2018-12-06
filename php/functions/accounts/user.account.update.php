e69f6a610a669c65f4f898330ebddc8ab52bee9f
Orlan Castelo <casteloorlan@gmail.com>
2018-12-06 16:05:35 +0800

    save

--- php/functions/accounts/user.account.update.php
+++ php/functions/accounts/user.account.update.php
@@ -107,7 +107,7 @@ if($status != $row['status'])
     
 $sqlqurey2 = "INSERT INTO tbl_history (transact_name, transact_details, transact_date, user_id)
              
-VALUES ('$EditUser', '$queryvar', NOW(), '$userid2')";
+VALUES ('$EditUser', 'Name:".$_POST["name"].",Department:".$_POST["department"].",Position:".$_POST["position"].",Role:".$_POST["role"].",Status:".$_POST["status"]."', NOW(), '$userid2')";
 ($db->query($sqlqurey2));
 }
 ?>
