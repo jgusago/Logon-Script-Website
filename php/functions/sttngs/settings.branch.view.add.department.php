2f2bc000fb74dadcd57105328291171fcf7fa738
Orlan Castelo <casteloorlan@gmail.com>
2018-12-06 16:48:40 +0800

    save

--- php/functions/sttngs/settings.branch.view.add.department.php
+++ php/functions/sttngs/settings.branch.view.add.department.php
@@ -7,6 +7,9 @@ $path = $_POST['path'];
 $dept = $_POST['dept'];
 $filter = $_POST['filter'];
 
+$AddDepartment = "Add Department";
+$userid2 = $_SESSION['userid'];
+
 if ($branch == $path){
 $length = 2;
 $parent = $branch;
@@ -25,6 +28,12 @@ if($existing == false){
 $query = "INSERT INTO logonscript.tbl_tree (tree_name, tree_level, tree_parent, tree_filter) VALUES ('$dept', $length, '$parent', '$filter')";
 $db->query($query);
 echo "true";
+
+$sqlqurey = "INSERT INTO tbl_history (transact_name, transact_details, transact_date, user_id)
+             
+VALUES ('$AddDepartment', 'Department:".$_POST["tree_name"].",Filter Department:".$_POST["tree_filter"]."', NOW(), '$userid2')";
+($db->query($sqlqurey));
+
 }
 else{
   echo "Error Have Been Accured";
