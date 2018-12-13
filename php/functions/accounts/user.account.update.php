7bc1fcb51510edf6149e779f4ded3aee7efea7ad
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-12-13 17:44:19 +0800

    Update user.account.update.php

--- php/functions/accounts/user.account.update.php
+++ php/functions/accounts/user.account.update.php
@@ -65,7 +65,7 @@ if($oldname !== $name && $name !== ""){
  }
 
  if ($oldpassword !== md5(sha1($password))){
-     //$password = password_hash($password, PASSWORD_DEFAULT); 
+        $password =  md5(sha1($password)); 
          $query = "UPDATE logonscript.tbl_user SET `password`='$password' WHERE (`userid` = '$userid')";
          $db->query($query);
          $namestat = true;
