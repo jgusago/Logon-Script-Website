3e3ae6777d90f9f4f9f678b534eda124970f2f3e
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-12-20 14:18:49 +0800

    try

--- js/controller/admin.js
+++ js/controller/admin.js
@@ -1049,7 +1049,9 @@ function Profiles()
     var card = [], cbd = [], div =[], ul =[], li =[], a =[], 
     
     divv =[], divvv = []
-    divs = [], divsUid = [], divslbl = [], divsUids = [];
+    divs = [], divsUid = [], divslbl = [], divsUids = [],
+
+    divs2 = [], divsUname =[], divslbl2 = [], divsUnames = [], hrUname = [];
 
     createCard(card, contentview, [], []);
     createnewElement([],card.head,"div",[],["id:chLabel"],"PROFILE INFORMATION");
@@ -1067,6 +1069,13 @@ function Profiles()
     createnewElement(divsUid, divs.newelement, "div", ["col-sm-3", "col-md-2", "col-5"], [], "");
     createnewElement(divslbl, divsUid.newelement, "label", [], ["id:lblEid"], "EMPLOYEE ID: ");
     createnewElement(divsUids, divs.newelement, "div", ["col-md-8", "col-6"], [], "1040359")
+    createnewElement(hrUid, divv.newelement, "hr", [], [], "");
+
+    createnewElement(divs2, divvv.newelement, "div", ["row"], [], "");
+    createnewElement(divsUname, divs2.newelement, "div", ["col-sm-3", "col-md-2", "col-5"], [], "");
+    createnewElement(divslbl2, divsUname.newelement, "label", [], ["id:lblEid"], "NAME: ");
+    createnewElement(divsUnames, divs2.newelement, "div", ["col-md-8", "col-6"], [], "Camille Kate")
+    createnewElement(hrUname, divv.newelement, "hr", [], [], "");
 
 
     // createnewElement(h6, card.body,"h6", [],[],"USER ID:");
