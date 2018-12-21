4c5a5af1a045c871f4c8add409eb247528be2b57
Orlan Castelo <casteloorlan@gmail.com>
2018-12-21 17:09:50 +0800

    Update settings.branch.view.edit.php

--- php/functions/sttngs/settings.branch.view.edit.php
+++ php/functions/sttngs/settings.branch.view.edit.php
@@ -3,6 +3,9 @@ $newname = $_POST['newname'];
 $newfilter = $_POST['newfilter'];
 $id = $_POST['id'];
 
+$AddTreeParent = "Add Tree Parent";
+$userid2 = $_SESSION['userid'];
+
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
 $query1 = "SELECT tree_name FROM logonscript.tbl_tree WHERE tree_id = $id";
@@ -12,6 +15,11 @@ foreach ($db->query($query1) as $row) {
 
 $query2 = "UPDATE logonscript.tbl_tree SET tree_parent = '$newname' WHERE tree_parent LIKE '$name'";
 if ($db->query($query2)) {
+  
+$sqlqurey = "INSERT INTO tbl_history (transact_name, transact_details, transact_date, user_id)
+
+VALUES ('$AddTreeParent', 'Tree Parent:".$newname.", NOW(), '$userid2')";
+($db->query($sqlqurey));
 }
 else{
 }
