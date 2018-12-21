7e0b0241237f610cc1aba753bdd34140ef68c74e
Orlan Castelo <casteloorlan@gmail.com>
2018-12-21 14:16:27 +0800

    Update login_confirm.php

--- php/functions/login/login_confirm.php
+++ php/functions/login/login_confirm.php
@@ -20,7 +20,7 @@ if (isset($_POST["userid"]) && isset($_POST["password"])){
             {
                 echo "failed:inactive";
             }
-            elseif($status == 'Active' && $password != md5(sha1($hashed_password))) {
+            elseif($status == 'Active') {
                 $_SESSION["userid"] = $row[0]['userid']; 
                 if($role == "ADMINISTRATOR")
                 {
