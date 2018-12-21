d39cc528b8850bc5dcbf1f9bac66120dcc5c2643
John Melvin Gusago <42765192+jgusago@users.noreply.github.com>
2018-12-21 14:59:58 +0800

    Update settings.branch.view.edit.php

--- php/functions/sttngs/settings.branch.view.edit.php
+++ php/functions/sttngs/settings.branch.view.edit.php
@@ -5,10 +5,17 @@ $id = $_POST['id'];
 
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
-$query1 = "SELECT * FROM logonscript.tbl_tree WHERE tree_id = $id";
-foreach ($db->query($sql) as $row) {
-  
+$query1 = "SELECT tree_name FROM logonscript.tbl_tree WHERE tree_id = $id";
+foreach ($db->query($query1) as $row) {
+  $name = $row['tree_name'];
 }
+
+$query2 = "UPDATE logonscript.tbl_tree SET tree_parent = '$newname' WHERE tree_parent LIKE '$name'";
+if ($db->query($query2)) {
+}
+else{
+}
+
 
 $query = "UPDATE logonscript.tbl_tree SET tree_name = '$newname', tree_filter = '$newfilter' WHERE tree_id = $id";
 
