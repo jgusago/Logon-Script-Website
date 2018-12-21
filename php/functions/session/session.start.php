28d0f1c78925f5d363923f8ec5351aa68cdb6bbc
Orlan Castelo <casteloorlan@gmail.com>
2018-12-21 15:32:04 +0800

    Update session.start.php

--- php/functions/session/session.start.php
+++ php/functions/session/session.start.php
@@ -1,14 +1,14 @@
 <?php
-  $userid = $_POST["userid"];
+  $username = $_POST["username"];
   $password = $_POST["password"];
   //$enc = md5(sha1($password));
   
-if (isset($_POST["userid"]) && isset($_POST["password"])){
+if (isset($_POST["username"]) && isset($_POST["password"])){
 
   require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
   $stmt = $db->prepare("SELECT * FROM logonscript.tbl_user WHERE userid like :userid");
-  $stmt->bindValue(':userid', $userid);
+  $stmt->bindValue(':userid', $username);
   $stmt->execute();
   $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
