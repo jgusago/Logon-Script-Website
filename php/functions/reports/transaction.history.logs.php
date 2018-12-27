feabeea597418f2a35f2c15bf2801014b2bec461
orlancastelo <casteloorlan@gmail.com>
2018-12-27 16:29:07 +0800

    save

---
+++ php/functions/reports/transaction.history.logs.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+	require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
+
+	echo "Transcat Name|Transact Details|Transact Date|User";
+
+	if ($_SESSION['role'] == "ADMINISTRATOR") 
+	{
+
+		$sql = "SELECT transact_name, transact_details, transact_date, user_id FROM tbl_history WHERE role<>'SUPER ADMIN'";
+
+		foreach ($db->query($sql) as $row) 
+		{
+
+			$userid = $row['transact_name'] ?: 'null';
+			$name = $row['transact_details'] ?: 'null';
+			$department = $row['transact_date'] ?: 'null';
+			$position = $row['user_id'] ?: 'null';
+
+			echo "#$transact_name|$transact_details|$transact_date|$user_id";
+		}
+	}
+	else
+	{
+		$sql = "SELECT transact_name, transact_details, transact_date, user_id FROM tbl_history";
+
+		foreach ($db->query($sql1) as $row) 
+		{
+
+			$userid = $row['transact_name'] ?: 'null';
+			$name = $row['transact_details'] ?: 'null';
+			$department = $row['transact_date'] ?: 'null';
+			$position = $row['user_id'] ?: 'null';
+
+			echo "#$transact_name|$transact_details|$transact_date|$user_id";
+		}
+	}
+	
+	$db = null;
+?>
