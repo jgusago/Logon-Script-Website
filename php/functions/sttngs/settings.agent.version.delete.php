a5cbb9872442978e4456b1d5b038d13516c93071
Cams22 <42762357+Cams22@users.noreply.github.com>
2019-01-04 11:12:48 +0800

    Update settings.agent.version.delete.php

--- php/functions/sttngs/settings.agent.version.delete.php
+++ php/functions/sttngs/settings.agent.version.delete.php
@@ -3,14 +3,14 @@ session_start();
 
 $version = $_POST['version'];
 $userid2 = $_SESSION['userid'];
-$user = $_SESSION['name'];
+$users = $_SESSION['name'];
 
 require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
 
 $query = "DELETE FROM logonscript.tbl_agent_version WHERE version = '$version'";
 if($db->query($query))
 {
-  $query2 = "INSERT INTO tbl_history (transact_name, transact_details, transact_date, user_id) VALUES ('Agent Version Deleted', 'Agent Version: ".$version."', NOW(), '$userid2,$user')";
+  $query2 = "INSERT INTO tbl_history (transact_name, transact_details, transact_date, user_id) VALUES ('Agent Version Deleted', 'Agent Version: ".$version."', NOW(), '$userid2,$users')";
   ($db->query($query2));
 
   echo "success";
