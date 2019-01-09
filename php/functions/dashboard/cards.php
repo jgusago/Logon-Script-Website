e17c9848449529c3d05bb8031d35b2f96bd12867
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2019-01-09 13:12:56 +0800

    update

--- php/functions/dashboard/cards.php
+++ php/functions/dashboard/cards.php
@@ -6,12 +6,12 @@ session_start();
 $dept = $_SESSION["department"];
 $query = "SELECT tree_filter FROM tbl_tree where tree_name LIKE '$dept'";
 foreach ($db->query($query) as $row) {
-$filter = $row["tree_filter"];
+echo $filter = $row["tree_filter"];
 }
 if(!isset($filter)){
   $filter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 }
-$query = "SELECT COUNT(distinct hostname) FROM logonscript.tbl_log WHERE iMonitor_Status = 'End Task' AND hostname like '$filter%'";
+$query = "SELECT COUNT(distinct hostname) FROM logonscript.tbl_log WHERE (connection_status not like 'ESTABLISHED' or iMonitor_Status not like 'running') AND hostname like '$filter%'";
 foreach ($db->query($query) as $row) {
 $endtask = $row["COUNT(distinct hostname)"];
 }
