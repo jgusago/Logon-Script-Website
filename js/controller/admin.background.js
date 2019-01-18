9d2fe97b038f82b25fe882e3ad056bd555866c04
Cams22 <42762357+Cams22@users.noreply.github.com>
2019-01-18 14:12:52 +0800

    try

--- js/controller/admin.background.js
+++ js/controller/admin.background.js
@@ -9,7 +9,7 @@ function tablecheck(mod, trig){
     var table  = document.getElementById("datalist");
 
     if(table == null){
-      contentview.innerHTML = "<table id=\"datalist\" class=\"table able-bordered table-striped\" module=\""+mod+"\" trigger=\""+trig+"\" style=\"width: 100%;\"></table>";
+      contentview.innerHTML = "<table id=\"datalist\" class=\"table table-bordered table-hover\" module=\""+mod+"\" trigger=\""+trig+"\" style=\"width: 100%;\"></table>";
       table  = document.getElementById("datalist");
       return false;
     }
@@ -21,7 +21,7 @@ function tablecheck(mod, trig){
         return true;
       }
       else{
-        contentview.innerHTML = "<table id=\"datalist\" class=\"table able-bordered\" module=\""+mod+"\" trigger=\""+trig+"\" style=\"width: 100%;\"></table>";
+        contentview.innerHTML = "<table id=\"datalist\" class=\"table table-bordered table-hover\" module=\""+mod+"\" trigger=\""+trig+"\" style=\"width: 100%;\"></table>";
         return false;
       }
     }
