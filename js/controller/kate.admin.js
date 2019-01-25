e46e2410efc7ab1626831b0b9c3032a6d4da9f4d
Cams22 <42762357+Cams22@users.noreply.github.com>
2019-01-25 10:38:34 +0800

    Update kate.admin.js

---
+++ js/controller/kate.admin.js
@@ -0,0 +1,60 @@
+function DSHBRDAccountsAccMgnt(){
+    var checktable = tablecheck("user accounts", "User Accounts");
+    var a = document.getElementById("ContentCardHead").innerHTML = "User Accounts";
+    var foot = document.getElementById("ContentCardFoot");
+  
+    if (checktable == false){
+  
+    $('#datalist').DataTable( {
+      dom: "<'row mt-2'<'col-sm-12 col-md-4'l><'col-sm-12 col-md-4'r><'col-sm-12 col-md-4'f>>"+
+        "<'row'<'col-sm-12'tr>>"+
+        "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",//lBfrtip
+      "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+      columns: [
+            { title: "User ID" },
+            { title: "Name" },
+            { title: "Department"},
+            { title: "Job Positionr" },
+            { title: "IP Address"},
+            { title: "Role"},
+            { title: "Status"},
+            { title: "Action"}
+          ],
+      "order": [[ 0, "desc" ]]
+    });
+  
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      Loading(true);
+      if (this.readyState == 4 && this.status == 200) {
+      myObj = JSON.parse(this.responseText);
+        for (x in myObj) {
+          if(myObj[x].name == null){
+            var user = myObj[x].userid;
+          }
+          else{
+            var user = myObj[x].userid+" - "+myObj[x].name;
+          }
+          $('#datalist').DataTable().row.add([
+            myObj[x].userid,
+            myObj[x].name,
+            myObj[x].department,
+            myObj[x].position,
+            myObj[x].role,
+            myObj[x].status,
+            // "button`btn~btn-primary`id:$userid-7~onclick:ACCTedit(\"$userid\",\"$name\",\"$department\",\"$position\",\"$role\",\"$status\")`Edit`";
+            "<button class=\"btn btn-primary\"onClick=\"ACCTedit()\">Details</button>"
+
+          ]).draw(false);
+        }//for close
+        Loading(false);
+    }//if close
+  }//function close
+    xmlhttp.open("POST", "php/functions/accounts/accounts.view.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send();
+  }
+  else{
+    //do nothing
+  }
+}
